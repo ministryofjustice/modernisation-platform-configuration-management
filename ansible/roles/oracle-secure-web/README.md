a89693d4fc7395ce35502e08d25caffe92bb8375
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-10-05 15:02:16 +0100

    nomis: DSOS-1507: fix oracle-secure-web ansible

--- ansible/roles/oracle-secure-web/README.md
+++ ansible/roles/oracle-secure-web/README.md
@@ -1,1 +1,7 @@
 Role for installing oracle secure web
+
+Force the installation handler to run by using osw-force-install tag, e.g.
+
+```
+ansible-playbook  -i inventory_aws_ec2.yml site.yml  --limit t1-nomis-db-2 -e role=oracle-secure-web  -v --tags=osw-force-install,ec2provision
+```
--- ansible/roles/oracle-secure-web/templates/install.sh
+++ ansible/roles/oracle-secure-web/templates/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:"{{ osw_install_dir }}/jdk1.7.0_80/bin"
-java -jar "{{ osw_install_dir }}/osbws_install.jar" -ARGFILE "{{ osw_install_dir }}/osbws_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" | logger -p local3.info -t java
+java -jar "{{ osw_install_dir }}/osbws_install.jar" -ARGFILE "{{ osw_install_dir }}/osbws_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" -useHttps | logger -p local3.info -t java
