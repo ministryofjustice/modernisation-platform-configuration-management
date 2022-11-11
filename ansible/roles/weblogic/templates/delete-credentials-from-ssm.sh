bee6bffad4299bbcd400b793e748e2b9001fbb77
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-11-11 15:38:53 +0000

    DSOS-1440: weblogic fixes

--- ansible/roles/weblogic/templates/delete-credentials-from-ssm.sh
+++ ansible/roles/weblogic/templates/delete-credentials-from-ssm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 PROFILE=$1
-DB_NAME=$2
+SSM_PATH=$2
 
-if [[ -z $PROFILE || -z $DB_NAME ]]; then
-  echo "Usage <profile> <environment>"
+if [[ -z $PROFILE || -z $SSM_PATH ]]; then
+  echo "Usage <profile> <ssm_path>"
   exit 1
 fi
 
-aws ssm delete-parameter --name "/weblogic/${DB_NAME}/WEBLOGIC_USERNAME" --profile "$PROFILE"
-aws ssm delete-parameter --name "/weblogic/${DB_NAME}/WEBLOGIC_PASSWORD" --profile "$PROFILE"
-aws ssm delete-parameter --name "/weblogic/${DB_NAME}/DB_USERNAME" --profile "$PROFILE"
-aws ssm delete-parameter --name "/weblogic/${DB_NAME}/DB_PASSWORD" --profile "$PROFILE"
+aws ssm delete-parameter --name "${SSM_PATH}/WEBLOGIC_USERNAME" --profile "$PROFILE"
+aws ssm delete-parameter --name "${SSM_PATH}/WEBLOGIC_PASSWORD" --profile "$PROFILE"
+aws ssm delete-parameter --name "${SSM_PATH}/DB_USERNAME" --profile "$PROFILE"
+aws ssm delete-parameter --name "${SSM_PATH}/DB_PASSWORD" --profile "$PROFILE"
