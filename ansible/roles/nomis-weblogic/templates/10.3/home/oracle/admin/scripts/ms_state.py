6919a060da4b8a2db9c8c5fe012de3cea85bbb7f
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-12-21 14:26:25 +0000

    DSOS-1614: nomis weblogic ansible tidy up

--- ansible/roles/nomis-weblogic/templates/10.3/home/oracle/admin/scripts/ms_state.py
+++ ansible/roles/nomis-weblogic/templates/10.3/home/oracle/admin/scripts/ms_state.py
@@ -2,40 +2,24 @@
 import getopt
 import sys
 import socket
-
 from java.io import FileInputStream
 
-propInputStream = FileInputStream(
-    "/home/oracle/admin/scripts/weblogic.properties")
+
+def conn():
+  try:
+    connect(url=adminURL, adminServerName=adminServerName)
+  except ConnectionException,e:
+    print 'Unable to find admin server'
+    exit()
+
+
+propInputStream = FileInputStream("/home/oracle/admin/scripts/weblogic.properties")
 configProps = Properties()
 configProps.load(propInputStream)
 adminURL = configProps.get("domain.adminurl")
-adminUsername = configProps.get("domain.adminUsername")
-adminPassword = configProps.get("domain.adminPassword")
 adminServerName = configProps.get("domain.adminServerName")
 
-
-def usage():
-    print('get_param.py -u <username> -p <password> -h <admin_hostname>')
-
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "u:p:h:d:n:", [
-                               "properties=", "password=", "hostname="])
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
-for opt, arg in opts:
-    if opt in ("-u", "--username"):
-        adminUsername = arg
-    elif opt in ("-p", "--password"):
-        adminPassword = arg
-    elif opt in ("-h", "--hostname"):
-        adminURL = arg + ':7001'
-
-# Connect to the AdminServer.
-connect(adminUsername, adminPassword, adminURL)
-
+conn()
 servers = cmo.getServers()
 print "-------------------------------------------------------"
 print "\t"+cmo.getName()+" domain status"
