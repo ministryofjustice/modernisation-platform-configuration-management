803edf5284f462b33303c700a7ad15820ebef58a
sandhya Gandalwar <sandhya.gandalwar@digital.justice.gov.uk>
2023-03-16 17:34:30 +0000

    Removed set log rotate step from create_managed_app.py

--- ansible/roles/nomis-weblogic/templates/10.3/home/oracle/admin/scripts/create_managed_app.py
+++ ansible/roles/nomis-weblogic/templates/10.3/home/oracle/admin/scripts/create_managed_app.py
@@ -109,25 +109,6 @@ def wait_for_ms_start():
             Thread.sleep(10000)
             continue
 
-# Function that sets up managed server log rotation
-
-
-def set_ms_log_rotation():
-    edit()
-    startEdit()
-    cd('/Servers/' + msName + '/Log/' + msName)
-    cmo.setRotationType('bySize')
-    cmo.setFileMinSize(50000)
-    cmo.setNumberOfFilesLimited(true)
-    cmo.setFileCount(10)
-    cmo.setRedirectStderrToServerLogEnabled(false)
-    cmo.setRedirectStdoutToServerLogEnabled(false)
-    cmo.setMemoryBufferSeverity('Debug')
-    cmo.setLogFileSeverity('Trace')
-    save()
-    activate()
-
-
 # Connect to the AdminServer.
 connect(adminUsername, adminPassword, adminURL)
 
@@ -163,7 +144,6 @@ if msName:
     # Start Managed Server
     start(msName, 'Server')
     wait_for_ms_start()
-    set_ms_log_rotation()
 
 # Create Data Source(s)
 if dsName:
