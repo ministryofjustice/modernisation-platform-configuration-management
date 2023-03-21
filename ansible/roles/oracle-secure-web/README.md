02c6e687d7a735476eec0a2634a86843e9f08419
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-03-21 16:27:46 +0000

    added in AMI code to oracle-secure-web

--- ansible/roles/oracle-secure-web/README.md
+++ ansible/roles/oracle-secure-web/README.md
@@ -1,7 +1,1 @@
 Role for configuring oracle secure web. The installation files are added as part of the AMI build.
-
-Force the installation handler to run by using osw-force-install tag, e.g.
-
-```
-ansible-playbook  -i inventory_aws_ec2.yml site.yml  --limit t1-nomis-db-2 -e role=oracle-secure-web  -v --tags=osw-force-install,ec2provision
-```
--- ansible/roles/oracle-secure-web/templates/install.sh
+++ ansible/roles/oracle-secure-web/templates/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PATH=$PATH:"{{ osw_install_dir }}/jdk1.7.0_80/bin"
-java -jar "{{ osw_install_dir }}/osbws_install.jar" -ARGFILE "{{ osw_install_dir }}/osbws_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" -useHttps | logger -p local3.info -t java
+export PATH=$PATH:"{{ osw_stage_dir }}/jdk1.7.0_80/bin"
+java -jar "{{ osw_stage_dir }}/osbws_install.jar" -ARGFILE "{{ osw_stage_dir }}/osbws_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" -useHttps | logger -p local3.info -t java
