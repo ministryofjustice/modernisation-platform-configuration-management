7700c078a83fe12e123edc57f293b897be35a11e
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-06-06 13:49:24 +0100

    change how healthcheck works

--- ansible/roles/nomis-weblogic/templates/10.3/etc/healthcheck/healthcheck.sh
+++ ansible/roles/nomis-weblogic/templates/10.3/etc/healthcheck/healthcheck.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Source function library.
+. /etc/init.d/functions
+
+keepalive() {
+   echo '<HTML><BODY>keepalive.htm on {{ weblogic_servername }}</BODY></HTML>'
+}
+
 while true
 do
-    service weblogic-all healthcheck
+    if ! weblogic-all healthcheck; then
+        echo "Removing keepalive" | logger -p local3.info -t "healthcheck"
+        rm -f /u01/tag/static/keepalive.htm
+    fi
+    echo "Creating keepalive /u01/tag/static/keepalive.htm" | logger -p local3.info -t "healthcheck"
+    keepalive > /u01/tag/static/keepalive.htm
+    chown oracle:oinstall /u01/tag/static/keepalive.htm
+    echo_success
+    echo
     sleep 120
 done
