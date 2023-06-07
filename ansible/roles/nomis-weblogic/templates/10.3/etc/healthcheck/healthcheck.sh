969025658b0ac97b0be58dbbcab20fafd4350a1b
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-06-07 12:03:51 +0100

    everything working bar the healthcheck if check

--- ansible/roles/nomis-weblogic/templates/10.3/etc/healthcheck/healthcheck.sh
+++ ansible/roles/nomis-weblogic/templates/10.3/etc/healthcheck/healthcheck.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Source function library.
-. /etc/init.d/functions
-
 keepalive() {
    echo '<HTML><BODY>keepalive.htm on {{ weblogic_servername }}</BODY></HTML>'
 }
 
 while true
 do
-    if ! service weblogic-all healthcheck > /dev/null 2>&1; then
+    /etc/init.d/weblogic-all healthcheck > /dev/null 2>&1
+    if [ $? -eq 1 ]
+    then
         echo "Removing keepalive" | logger -p local3.info -t "healthcheck"
         rm -f /u01/tag/static/keepalive.htm
+    else
+        echo "Creating keepalive /u01/tag/static/keepalive.htm" | logger -p local3.info -t "healthcheck"
+        keepalive > /u01/tag/static/keepalive.htm
+        chown oracle:oinstall /u01/tag/static/keepalive.htm
+        echo
     fi
-    echo "Creating keepalive /u01/tag/static/keepalive.htm" | logger -p local3.info -t "healthcheck"
-    keepalive > /u01/tag/static/keepalive.htm
-    chown oracle:oinstall /u01/tag/static/keepalive.htm
-    echo_success
-    echo
-    sleep 120
+    sleep 30
 done
