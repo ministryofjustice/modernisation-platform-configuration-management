a52974621cc601c5644352eaad9a1485d6c9f7b4
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-09 16:29:42 +0000

    fix

--- powershell/Scripts/Run-GitScript.ps1
+++ powershell/Scripts/Run-GitScript.ps1
@@ -29,11 +29,9 @@ $GitOrg = "ministryofjustice"
 $GitRepo = "modernisation-platform-configuration-management"
 
 if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
-  Write-Error "Please install git, e.g. choco install git or brew install git"
+  Write-Error "Please install git, e.g. choco install git.install -y"
   exit 1
 }
-
-git config core.longpaths true
 
 if (-Not $GitCloneDir) {
   $GitCloneDir = [System.IO.Path]::GetTempPath()
@@ -43,7 +41,7 @@ $env:GIT_REDIRECT_STDERR="2>&1"
 Set-Location -Path $GitCloneDir
 if (-not (Test-Path -Path $GitRepo)) {
   Write-Output "git clone https://github.com/${GitOrg}/${GitRepo}.git into $GitCloneDir"
-  git clone "https://github.com/${GitOrg}/${GitRepo}.git"
+  git clone -c core.longpaths=true "https://github.com/${GitOrg}/${GitRepo}.git"
   Set-Location -Path $GitRepo
 } else {
   Set-Location -Path $GitRepo
