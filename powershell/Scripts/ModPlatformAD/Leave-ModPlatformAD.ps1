fac2afd654052e64a92bb3931508c84f5f125ac0
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-09 10:04:12 +0000

    add scripts

---
+++ powershell/Scripts/ModPlatformAD/Leave-ModPlatformAD.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Unjoin computer from current Mod Platform domain
+
+.DESCRIPTION
+    Either pass in the doman name as a parameter, or derive the AD configuration 
+    from EC2 tags (environment-name or domain-name).          
+    EC2 requires permissions to get tags and the aws cli.
+    Exits with 3010 if reboot required and script requires re-running. For use in SSM docs
+
+.PARAMETER DomainNameFQDN
+    Optionally specify the FQDN of the domain name to join
+
+.EXAMPLE
+    Leave-ModPlatformAD
+
+.OUTPUTS
+    HashTable
+#>
+
+[CmdletBinding()]
+param (
+  [string]$DomainNameFQDN
+  [string]$AccountIdsSSMParameterName = "account_ids"
+)
+
+Import-Module ModPlatformADConfig
+Import-Module ModPlatformADCredential
+Import-Module ModPlatformADComputer
+
+$ErrorActionPreference = "Stop"
+
+$ADConfig = Get-ModPlatformADConfig -DomainNameFQDN $DomainNameFQDN
+$ADCredential = Get-ModPlatformADCredential -ModPlatformADConfig $ADConfig -AccountIdsSSMParameterName $AccountIdsSSMParameterName
+if (Remove-ModPlatformADComputer -$ModPlatformADConfig $ADConfig -ModPlatformADCredential $ADCredential) {
+  exit 3010 # triggers reboot if running from SSM Doc
+}
