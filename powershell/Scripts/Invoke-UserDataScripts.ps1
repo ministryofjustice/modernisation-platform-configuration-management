40c414043d828d444a73947036e503c9dc4a3f9a
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-12 10:56:58 +0000

    add user-data script

---
+++ powershell/Scripts/Invoke-UserDataScripts.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    Get server-type tag and run associated UserDataScript
+#>
+
+$ErrorActionPreference = "Stop"
+$Token = Invoke-RestMethod -TimeoutSec 2 -Headers @{"X-aws-ec2-metadata-token-ttl-seconds"=3600} -Method PUT -Uri http://169.254.169.254/latest/api/token
+$InstanceId = Invoke-RestMethod -TimeoutSec 2 -Headers @{"X-aws-ec2-metadata-token" = $Token} -Method GET -Uri http://169.254.169.254/latest/meta-data/instance-id
+$TagsRaw = aws ec2 describe-tags --filters "Name=resource-id,Values=$InstanceId"
+$Tags = "$TagsRaw" | ConvertFrom-Json
+$ServerTypeTag = ($Tags.Tags | Where-Object  {$_.Key -eq "server-type"}).Value
+$Script = ". ServerType/${ServerTypeTag}.ps1"
+
+if (-not $ServerTypeTag) {
+  Write-Error "Missing or blank server-type tag"
+} elseif (-not (Get-ChildItem $Script -ErrorAction SilentlyContinue)) {
+  Write-Error "Could not find $Script"
+} else {
+  . $Script
+}
