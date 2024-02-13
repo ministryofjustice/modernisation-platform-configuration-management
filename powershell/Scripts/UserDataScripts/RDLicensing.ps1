389b97d6bbcfe8ee7078a2f82b315cfaf9c38bf2
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-13 16:40:07 +0000

    fix

--- powershell/Scripts/UserDataScripts/RDLicensing.ps1
+++ powershell/Scripts/UserDataScripts/RDLicensing.ps1
@@ -1,10 +1,10 @@
-Import ModPlatformRemoteDesktop -Force
+Import-Module ModPlatformRemoteDesktop -Force
 
 # Join Domain
 . ../ModPlatformAD/Join-ModPlatformAD.ps1
 
 # Install RD Licensing component and activate
 $ErrorActionPreference = "Stop"
-Install-WindowsFeature RDS-Licensing –IncludeAllSubFeature -IncludeManagementTools
+Install-WindowsFeature RDS-Licensing -IncludeAllSubFeature -IncludeManagementTools
 $CompanyInformation = Get-ModPlatformRDLicensingCompanyInformation
 Add-ModPlatformRDLicensingActivation $CompanyInformation
