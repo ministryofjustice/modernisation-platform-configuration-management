2edb9e6682a4cb156847520371c94ce9e80321a3
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-03-08 16:16:49 +0000

    DSOS-2635: powershell ad updates for new DC (#600)
    * fix typo
    
    * Rename Get-ModPlatformADCredential function
    
    * Add additional AD Credential functions
    
    * test
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Fix
    
    * update

--- powershell/Modules/ModPlatformAD/README.md
+++ powershell/Modules/ModPlatformAD/README.md
@@ -3,9 +3,9 @@ Provides Active Directory related functions
 For example,
 
 ```
-Get-ModPlatformADConfig      - derive AD config from environment
-Get-ModPlatformADCredential  - lookup domain join secret
-Rename-ModPlatformADComputer - rename computer
-Add-ModPlatformADComputer    - add to domain
-Remove-ModPlatformADComputer - remove from domain
+Get-ModPlatformADConfig          - derive AD config from environment
+Get-ModPlatformADJoinCredential  - lookup domain join secret
+Rename-ModPlatformADComputer     - rename computer
+Add-ModPlatformADComputer        - add to domain
+Remove-ModPlatformADComputer     - remove from domain
 ```
---
+++ powershell/Scripts/ModPlatformAD/Uninstall-ModPlatformADDomainController.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Install a Domain Controller from scratch
+
+.DESCRIPTION
+    By default the script derives the hostname from the Name tag. Or specify NewHostname parameter.
+    By default derives the AD configuration from EC2 tags (environment-name or domain-name), or specify DomainNameFQDN parameter.
+    EC2 requires permissions to get tags and the aws cli.
+    Exits with 3010 if reboot required and script requires re-running. For use in SSM docs
+    Example retrieval of local admin password:
+      aws ssm get-parameter --name ec2-user_pem --with-decryption --query Parameter.Value --output text --profile hmpps-domain-services-test > tmp.key
+      aws ec2 get-password-data --instance-id i-0aa02abedd9572e19 --profile core-shared-services-production-ad --priv-launch-key tmp.key
+      rm tmp.key
+
+.PARAMETER DomainNameFQDN
+    Optionally specify the FQDN of the domain name to join
+
+.EXAMPLE
+    Join-ModPlatformAD
+#>
+
+[CmdletBinding()]
+param (
+  [string]$NewHostname = "tag:Name",
+  [string]$DomainNameFQDN
+)
+
+Import-Module ModPlatformAD -Force
+
+$ErrorActionPreference = "Stop"
+
+$ADConfig = Get-ModPlatformADConfig -DomainNameFQDN $DomainNameFQDN
+$ADSecret = Get-ModPlatformADSecret -ModPlatformADConfig $ADConfig
+
+$DFSReplicationStatus = Get-Service "DFS Replication" -ErrorAction SilentlyContinue
+if ($DFSReplicationStatus -ne $null) {
+  $ADAdminCredential = Get-ModPlatformADAdminCredential -ModPlatformADConfig $ADConfig -ModPlatformADSecret $ADSecret
+  $ADSafeModeAdministratorPassword = Get-ModPlatformADSafeModeAdministratorPassword -ModPlatformADConfig $ADConfig -ModPlatformADSecret $ADSecret
+  Uninstall-ADDSDomainController -Credential $ADAdminCredential -NoRebootOnCompletion -Force
+  Exit 3010 # triggers reboot if running from SSM Doc
+}
