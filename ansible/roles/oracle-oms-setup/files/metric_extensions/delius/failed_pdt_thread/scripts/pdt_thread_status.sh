281093d22fcf8ad48ebfe95e28ca4cbe7a5f7aaf
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-05-14 14:30:13 +0100

    Fix naming in error message

--- ansible/roles/oracle-oms-setup/files/metric_extensions/delius/failed_pdt_thread/scripts/pdt_thread_status.sh
+++ ansible/roles/oracle-oms-setup/files/metric_extensions/delius/failed_pdt_thread/scripts/pdt_thread_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Get status code for PRF threads (2=failed)
+#  Get status code for PDT threads (2=failed)
 #
 
 . ~/.bash_profile
@@ -15,7 +15,7 @@ SET HEAD OFF
 SET PAGES 0
 SELECT component_id||'|'||thread_id||'|'||status
 FROM delius_app_schema.pdt_thread 
-WHERE component_id BETWEEN 500 AND 505   -- Performance Monitoring Threads
+WHERE component_id BETWEEN 500 AND 505   -- Performance Monitoring Threads (PRF)
 OR         component_id BETWEEN 100 AND 107   -- Alfresco Message Queue Threads
 ORDER BY component_id, thread_id;
 EXIT
