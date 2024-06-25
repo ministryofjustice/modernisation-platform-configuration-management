472c1930bd35587719871c948b2fd7cf326aef38
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-06-25 16:17:50 +0100

    rename file from .ini to .rsp

--- powershell/Scripts/UserDataScripts/NcrClient.ps1
+++ powershell/Scripts/UserDataScripts/NcrClient.ps1
@@ -98,7 +98,7 @@ Expand-Installer -File ( ".\" + $Config.DataServicesS3File) -Destination ".\Data
 Set-Location -Path $WorkingDirectory/Client
 # documentation: https://docs.oracle.com/en/database/oracle/oracle-database/19/ntcli/running-oracle-universal-installe-using-the-response-file.html
 # FIXME file name needs fixing
-setup.exe -silent -noconfig -nowait -responseFile ($ConfigurationManagementRepo + "\powershell\Configs\NCROracle19Response.ini")
+setup.exe -silent -noconfig -nowait -responseFile ($ConfigurationManagementRepo + "\powershell\Configs\NCROracle19Response.rsp")
 # }}}
 
 # {{{ login text
