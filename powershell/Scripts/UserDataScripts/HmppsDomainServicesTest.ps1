fa050bbc6c37e6b180eaf6ff80b7b109020910b2
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-03 16:49:26 +0100

    test RDGateway code

--- powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
+++ powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
@@ -1,2 +1,34 @@
+$ErrorActionPreference = "Stop"
+
 . ../ModPlatformAD/Join-ModPlatformAD.ps1
+
+if ($LASTEXITCODE -ne 0) {
+   Exit $LASTEXITCODE
+}
+
+$DomainNameNetbios = $ADConfig.DomainNameNetbios
+
+$CAP = @{
+  "Name" = "default"
+  "AuthMethod" = 1
+  "Status" = 1
+  "IdleTimeout" = 120
+  "SessionTimeout" = 480
+  "SessionTimeoutAction" = 0
+  "UserGroups" = "Domain Users@${DomainNameNetbios}"
+}
+$RAP = @{
+  "Name" = "default"
+  "ComputerGroupType" = 2
+  "UserGroups" = "Domain Users@${DomainNameNetbios}"
+}
+
+Import-Module ModPlatformRemoteDesktop -Force
+
+Add-ModPlatformRDGateway
+Set-ModPlatformRDGatewayCAP @CAP
+Set-ModPlatformRDGatewayRAP @RAP
+
+. ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
+
 Exit $LASTEXITCODE
