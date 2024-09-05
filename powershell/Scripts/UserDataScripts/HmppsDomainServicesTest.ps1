38bbb2cdae3a8530d356fc1b85e67b082fe49901
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-05 13:26:01 +0100

    fix

--- powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
+++ powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
@@ -1,33 +1,10 @@
 $ErrorActionPreference = "Stop"
 
-. ../ModPlatformAD/Join-ModPlatformAD.ps1
+. ../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname "keep-existing"
 
 if ($LASTEXITCODE -ne 0) {
    Exit $LASTEXITCODE
 }
-
-$DomainNameNetbios = $ADConfig.DomainNameNetbios
-
-$CAP = @{
-  "Name" = "default"
-  "AuthMethod" = 1
-  "Status" = 1
-  "IdleTimeout" = 120
-  "SessionTimeout" = 480
-  "SessionTimeoutAction" = 0
-  "UserGroups" = "Domain Users@${DomainNameNetbios}"
-}
-$RAP = @{
-  "Name" = "default"
-  "ComputerGroupType" = 2
-  "UserGroups" = "Domain Users@${DomainNameNetbios}"
-}
-
-Import-Module ModPlatformRemoteDesktop -Force
-
-Add-ModPlatformRDGateway
-Set-ModPlatformRDGatewayCAP @CAP
-Set-ModPlatformRDGatewayRAP @RAP
 
 . ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
 
