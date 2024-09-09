eea891e14e3dbc005382707f88a0cc0ff1b8a7a7
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-09 12:06:17 +0100

    fixes

--- powershell/Scripts/UserDataScripts/RDServices.ps1
+++ powershell/Scripts/UserDataScripts/RDServices.ps1
@@ -5,26 +5,26 @@
 $GlobalConfig = @{
   "test-win-2022" = @{
     "ConnectionBroker" = "$env:computername.$env:userdnsdomain"
-    "LicensingServer" = "ad-azure-rdlic.azure.noms.root"
+    "LicensingServer" = "AD-AZURE-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.test.hmpps-domain.service.justice.gov.uk"
-    "RDSessionHosts" = @("EC2AMAZ-9T2GN5H.azure.noms.root")
+    "SessionHosts" = @("EC2AMAZ-9T2GN5H.AZURE.NOMS.ROOT")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
   }
   "pp-rds" = @{
     "ConnectionBroker" = "$env:computername.$env:userdnsdomain"
-    "LicensingServer" = "ad-hmpp-rdlic.azure.noms.root"
+    "LicensingServer" = "AD-HMPP-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.preproduction.hmpps-domain.service.justice.gov.uk"
-    "RDSessionHosts" = @("x.azure.hmpp.root")
+    "SessionHosts" = @("x.azure.hmpp.root")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
   }
   "pd-rds" = @{
     "ConnectionBroker" = "$env:computername.$env:userdnsdomain"
-    "LicensingServer" = "ad-hmpp-rdlic.azure.noms.root"
+    "LicensingServer" = "AD-HMPP-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.hmpps-domain.service.justice.gov.uk"
-    "RDSessionHosts" = @("x.azure.hmpp.root")
+    "SessionHosts" = @("x.azure.hmpp.root")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
   }
 }
@@ -45,7 +45,7 @@ function Get-Config {
 function Install-RDSWindowsFeatures {
   [CmdletBinding()]
   param (
-    [string[]]$Features = ("RDS-Gateway", "RDS-Connection-Broker", "RDS-Web-Access")
+    [string[]]$Features = ("RDS-GATEWAY", "RDS-CONNECTION-BROKER", "RDS-WEB-ACCESS")
   )
   $Features | ForEach-Object {
     if (-not (Get-WindowsFeature -Name $_).Installed) {
@@ -62,12 +62,12 @@ function Add-RDSessionDeployment {
     [string[]]$SessionHosts,
     [string]$WebAccessServer
   )
-  if (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Connection-Broker -ErrorAction SilentlyContinue) {
+  if (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-CONNECTION-BROKER -ErrorAction SilentlyContinue) {
     Add-RDWebAccessServer -ConnectionBroker $ConnectionBroker -WebAccessServer $WebAccessServer
-    # Add-RDSessionHosts -ConnectionBroker $ConnectionBroker -Servers $SessionHosts
+    Add-SessionHosts -ConnectionBroker $ConnectionBroker -SessionHosts $SessionHosts
   } else {
     Write-Output "${ConnectionBroker}: Creating new RDSession Deployment"
-    New-RDSessionDeployment -ConnectionBroker $ConnectionBroker -SessionHost $SessionHosts -WebAccessServer $WebAccessServer
+    DSessionDeployment -ConnectionBroker $ConnectionBroker -SessionHost $SessionHosts -WebAccessServer $WebAccessServer
   }
 }
 
@@ -78,9 +78,9 @@ function Add-RDLicensingServer {
     [string]$LicensingServer
   )
 
-  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Licensing | Where-Object -Property Server -EQ $LicensingServer)) {
-    Write-Output "${LicensingServer}: Adding RDS-Licensing Server"
-    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $LicensingServer -Role RDS-Licensing
+  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-LICENSING | Where-Object -Property Server -EQ $LicensingServer)) {
+    Write-Output "${LicensingServer}: Adding RDS-LICENSING Server"
+    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $LicensingServer -Role RDS-LICENSING
   }
 
   if ((Get-RDLicenseConfiguration -ConnectionBroker $ConnectionBroker).Mode -ne 'PerUser') {
@@ -96,9 +96,9 @@ function Remove-RDLicensingServer {
     [string]$LicensingServerToKeep
   )
 
-  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Licensing | Where-Object -Property Server -NE $LicensingServerToKeep | ForEach-Object {
-    Write-Output ($_.Server + ": Removing RDS-Licensing Server")
-    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-Licensing -Force
+  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-LICENSING | Where-Object -Property Server -NE $LicensingServerToKeep | ForEach-Object {
+    Write-Output ($_.Server + ": Removing RDS-LICENSING Server")
+    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-LICENSING -Force
   }
 }
 
@@ -110,9 +110,9 @@ function Add-RDGatewayServer {
     [string]$GatewayExternalFqdn
   )
 
-  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Gateway | Where-Object -Property Server -EQ $GatewayServer)) {
-    Write-Output "${GatewayServer}: Adding RDS-Gateway Server"
-    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $GatewayServer -Role RDS-Gateway -GatewayExternalFqdn $GatewayExternalFqdn
+  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-GATEWAY | Where-Object -Property Server -EQ $GatewayServer)) {
+    Write-Output "${GatewayServer}: Adding RDS-GATEWAY Server"
+    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $GatewayServer -Role RDS-GATEWAY -GatewayExternalFqdn $GatewayExternalFqdn
   }
 
   # TODO use Set-RDDeploymentGatewayConfiguration to update GatewayFQDN
@@ -125,9 +125,9 @@ function Remove-RDGatewayServer {
     [string]$GatewayServerToKeep
   )
 
-  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Gateway | Where-Object -Property Server -NE $GatewayServerToKeep | ForEach-Object {
-    Write-Output ($_.Server + ": Removing RDS-Gateway Server")
-    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-Gateway -Force
+  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-GATEWAY | Where-Object -Property Server -NE $GatewayServerToKeep | ForEach-Object {
+    Write-Output ($_.Server + ": Removing RDS-GATEWAY Server")
+    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-GATEWAY -Force
   }
 }
 
@@ -138,9 +138,9 @@ function Add-RDWebAccessServer {
     [string]$WebAccessServer
   )
 
-  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Web-Access | Where-Object -Property Server -EQ $WebAccessServer)) {
-    Write-Output "${WebAccessServer}: Adding RDS-Web-Access Server"
-    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $WebAccessServer -Role RDS-Web-Access
+  if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-WEB-ACCESS | Where-Object -Property Server -EQ $WebAccessServer)) {
+    Write-Output "${WebAccessServer}: Adding RDS-WEB-ACCESS Server"
+    Add-RDServer -ConnectionBroker $ConnectionBroker -Server $WebAccessServer -Role RDS-WEB-ACCESS
   }
 }
 
@@ -151,9 +151,37 @@ function Remove-RDWebAccessServer {
     [string]$WebAccessServerToKeep
   )
 
-  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-Web-Access | Where-Object -Property Server -NE $WebAccessServerToKeep | ForEach-Object {
-    Write-Output ($_.Server + ": Removing RDS-Web-Access Server")
-    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-Web-Access -Force
+  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-WEB-ACCESS | Where-Object -Property Server -NE $WebAccessServerToKeep | ForEach-Object {
+    Write-Output ($_.Server + ": Removing RDS-WEB-ACCESS Server")
+    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-WEB-ACCESS -Force
+  }
+}
+
+function Add-SessionHosts {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [string[]]$SessionHosts
+  )
+
+  foreach ($SessionHost in $SessionHosts) {
+    if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-RD-SERVER | Where-Object -Property Server -EQ $SessionHost)) {
+      Write-Output "${SessionHost}: Adding RDS-RD-SERVER Server"
+      Add-RDServer -ConnectionBroker $ConnectionBroker -Server $SessionHost -Role RDS-RD-SERVER
+    }
+  }
+}
+
+function Remove-SessionHosts {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [string[]]$SessionHostsToKeep
+  )
+
+  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-RD-SERVER | Where-Object -Property Server -notin $SessionHostsToKeep | ForEach-Object {
+    Write-Output ($_.Server + ": Removing RDS-RD-SERVER Server")
+    Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-RD-SERVER -Force
   }
 }
 
@@ -161,10 +189,11 @@ $ErrorActionPreference = "Stop"
 $Config = Get-Config
 
 Install-RDSWindowsFeatures
-Add-RDSessionDeployment -ConnectionBroker $Config.ConnectionBroker -SessionHosts $Config.RDSessionHosts -WebAccessServer $Config.WebAccessServer
+Add-RDSessionDeployment -ConnectionBroker $Config.ConnectionBroker -SessionHosts $Config.SessionHosts -WebAccessServer $Config.WebAccessServer
 Add-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServer $Config.LicensingServer
 Add-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServer $Config.GatewayServer -GatewayExternalFqdn $Config.GatewayExternalFqdn
 
-Remove-RDWebAccessServer -ConnectionBroker $Config.ConnectionBroker -WebAccessServerToKeep $WebAccessServer
+Remove-RDWebAccessServer -ConnectionBroker $Config.ConnectionBroker -WebAccessServerToKeep $Config.WebAccessServer
 Remove-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServerToKeep $Config.GatewayServer
 Remove-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServerToKeep $Config.LicensingServer
+Remove-SessionHosts -ConnectionBroker $Config.ConnectionBroker -SessionHostsToKeep $Config.SessionHosts
