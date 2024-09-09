5c20c0a0587bd20075b4b2af4c8ca644fe004197
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-09 17:46:37 +0100

    fix

--- powershell/Scripts/UserDataScripts/RDServices.ps1
+++ powershell/Scripts/UserDataScripts/RDServices.ps1
@@ -8,15 +8,31 @@ $GlobalConfig = @{
     "LicensingServer" = "AD-AZURE-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.test.hmpps-domain.service.justice.gov.uk"
-    "SessionHosts" = @("EC2AMAZ-M5FEA4N.AZURE.NOMS.ROOT")
+    "SessionHostServers" = @("EC2AMAZ-M5FEA4N.AZURE.NOMS.ROOT")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
+    "Collections" = @{
+      "CAFM-RDP" = @{
+        "SessionHosts" = @("EC2AMAZ-M5FEA4N.AZURE.NOMS.ROOT")
+        "Configuration" = @{
+          "CollectionDescription" = "PlanetFM RemoteDesktop App Collection 2"
+          "UserGroup" = @("azure\drobinson")
+        }
+      }
+    }
+    "RemoteApps" = @{
+      "Calc" = @{
+        "CollectionName" = "CAFM-RDP"
+        "DisplayName" = "Calc2022"
+        "FilePath" = 'C:\Windows\System32\win32calc.exe'
+      }
+    }
   }
   "pp-rds" = @{
     "ConnectionBroker" = "$env:computername.$env:userdnsdomain"
     "LicensingServer" = "AD-HMPP-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.preproduction.hmpps-domain.service.justice.gov.uk"
-    "SessionHosts" = @("x.azure.hmpp.root")
+    "SessionHostServers" = @("x.azure.hmpp.root")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
   }
   "pd-rds" = @{
@@ -24,7 +40,7 @@ $GlobalConfig = @{
     "LicensingServer" = "AD-HMPP-RDLIC.AZURE.NOMS.ROOT"
     "GatewayServer" = "$env:computername.$env:userdnsdomain"
     "GatewayExternalFqdn" = "rdgateway2.hmpps-domain.service.justice.gov.uk"
-    "SessionHosts" = @("x.azure.hmpp.root")
+    "SessionHostServers" = @("x.azure.hmpp.root")
     "WebAccessServer" = "$env:computername.$env:userdnsdomain"
   }
 }
@@ -59,15 +75,15 @@ function Add-RDSessionDeployment {
   [CmdletBinding()]
   param (
     [string]$ConnectionBroker,
-    [string[]]$SessionHosts,
+    [string[]]$SessionHostServers,
     [string]$WebAccessServer
   )
   if (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-CONNECTION-BROKER -ErrorAction SilentlyContinue) {
     Add-RDWebAccessServer -ConnectionBroker $ConnectionBroker -WebAccessServer $WebAccessServer
-    Add-SessionHosts -ConnectionBroker $ConnectionBroker -SessionHosts $SessionHosts
+    Add-SessionHostServer -ConnectionBroker $ConnectionBroker -SessionHostServers $SessionHostServers
   } else {
     Write-Output "${ConnectionBroker}: Creating new RDSession Deployment"
-    DSessionDeployment -ConnectionBroker $ConnectionBroker -SessionHost $SessionHosts -WebAccessServer $WebAccessServer
+    New-RDSessionDeployment -ConnectionBroker $ConnectionBroker -SessionHost $SessionHostServers -WebAccessServer $WebAccessServer
   }
 }
 
@@ -157,14 +173,14 @@ function Remove-RDWebAccessServer {
   }
 }
 
-function Add-SessionHosts {
+function Add-SessionHostServer {
   [CmdletBinding()]
   param (
     [string]$ConnectionBroker,
-    [string[]]$SessionHosts
+    [string[]]$SessionHostServers
   )
 
-  foreach ($SessionHost in $SessionHosts) {
+  foreach ($SessionHost in $SessionHostServers) {
     if (-not (Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-RD-SERVER | Where-Object -Property Server -EQ $SessionHost)) {
       Write-Output "${SessionHost}: Adding RDS-RD-SERVER Server"
       Add-RDServer -ConnectionBroker $ConnectionBroker -Server $SessionHost -Role RDS-RD-SERVER
@@ -172,16 +188,122 @@ function Add-SessionHosts {
   }
 }
 
-function Remove-SessionHosts {
+function Remove-SessionHostServer {
   [CmdletBinding()]
   param (
     [string]$ConnectionBroker,
-    [string[]]$SessionHostsToKeep
+    [string[]]$SessionHostServersToKeep
   )
 
-  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-RD-SERVER | Where-Object -Property Server -notin $SessionHostsToKeep | ForEach-Object {
+  Get-RDServer -ConnectionBroker $ConnectionBroker -Role RDS-RD-SERVER | Where-Object -Property Server -notin $SessionHostServersToKeep | ForEach-Object {
     Write-Output ($_.Server + ": Removing RDS-RD-SERVER Server")
     Remove-RDServer -ConnectionBroker $ConnectionBroker -Server $_.Server -Role RDS-RD-SERVER -Force
+  }
+}
+
+function Add-Collection {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [string]$CollectionName,
+    [hashtable[]]$Collection
+  )
+
+  $ExistingCollection = Get-RDSessionCollection -ConnectionBroker $ConnectionBroker | Where-Object -Property CollectionName -eq $CollectionName
+  if (-not $ExistingCollection) {
+    # ErrorAction set to Continue as errors are generated re GroupPolicy managed options
+    Write-Output "${ConnectionBroker}: ${CollectionName}: Creating RDSessionCollection"
+    New-RDSessionCollection -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName -SessionHost $Collection.SessionHosts -ErrorAction Continue
+  } else {
+    foreach ($SessionHost in $Collection.SessionHosts) {
+      $ExistingSessionHost = Get-RDSessionHost -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName | Where-Object -Property SessionHost -eq $SessionHost
+      if (-not $ExistingSessionHost) {
+        Write-Output "${ConnectionBroker}: ${CollectionName}: ${SessionHost}: Adding RDSessionHost"
+        Add-RDSessionHost -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName -SessionHost $SessionHost -ErrorAction Continue
+      }
+    }
+  }
+  Write-Output "${ConnectionBroker}: ${CollectionName}: Updating RDSessionCollection Configuration"
+  $Configuration = $Collection.Configuration
+  Set-RDSessionCollectionConfiguration @Configuration -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName
+}
+
+function Add-Collections {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [hashtable]$Collections
+  )
+
+  foreach ($CollectionName in $Collections.Keys) {
+    Add-Collection -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName -Collection $Collections[$CollectionName]
+  }
+}
+
+function Remove-Collections {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [hashtable]$CollectionsToKeep
+  )
+
+  $CollectionNamesToKeep = $CollectionsToKeep.Keys
+  Get-RDSessionCollection -ConnectionBroker $ConnectionBroker | Where-Object -Property CollectionName -notin $CollectionNamesToKeep | ForEach-Object {
+    Write-Output ("${ConnectionBroker}: " + $_.CollectionName + ": Removing RDSessionCollection")
+    Remove-RDSessionCollection -ConnectionBroker $ConnectionBroker -CollectionName $_.CollectionName -Force
+  }
+
+  foreach ($CollectionName in $CollectionsToKeep.Keys) {
+    $Collection = $CollectionsToKeep[$CollectionName]
+    Get-RDSessionHost -ConnectionBroker $ConnectionBroker -CollectionName $CollectionName | Where-Object -Property SessionHost -notin $Collection.SessionHosts | ForEach-Object {
+      Write-Output ("${ConnectionBroker}: ${CollectionName}: " + $_.SessionHost + ": Removing RDSessionHost from RDSessionCollection")
+      Remove-RDSessionHost -ConnectionBroker $ConnectionBroker -SessionHost $_.SessionHost -Force
+    }
+  }
+}
+
+function Add-RemoteApp {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [string]$Alias,
+    [hashtable]$Configuration
+  )
+
+  $CollectionName = $Configuration.CollectionName
+  $ExistingApp = Get-RDRemoteApp -ConnectionBroker $ConnectionBroker | Where-Object -Property Alias -eq $Alias
+  if (-not $ExistingApp) {
+    Write-Output "${ConnectionBroker}: ${CollectionName}: ${Alias}: Creating RDRemoteApp"
+    New-RDRemoteApp @Configuration -ConnectionBroker $ConnectionBroker -Alias $Alias
+  } else {
+    Write-Output "${ConnectionBroker}: ${CollectionName}: ${Alias}: Updating RDRemoteApp"
+    Set-RDRemoteApp @Configuration -ConnectionBroker $ConnectionBroker -Alias $Alias
+  }
+}
+
+function Add-RemoteApps {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [hashtable]$RemoteApps
+  )
+
+  foreach ($Alias in $RemoteApps.Keys) {
+    Add-RemoteApp -ConnectionBroker $ConnectionBroker -Alias $Alias -Configuration $RemoteApps[$Alias]
+  }
+}
+
+function Remove-RemoteApps {
+  [CmdletBinding()]
+  param (
+    [string]$ConnectionBroker,
+    [hashtable]$RemoteAppsToKeep
+  )
+
+  $AliasesToKeep = $RemoteAppsToKeep.Keys
+  Get-RDRemoteApp -ConnectionBroker $ConnectionBroker | Where-Object -Property Alias -notin $AliasesToKeep | ForEach-Object {
+    Write-Output ("${ConnectionBroker}: " + $_.CollectionName +": " + $_.Alias + ": Removing RDRemoteApp")
+    Remove-RDRemoteApp -ConnectionBroker $ConnectionBroker -CollectionName $_.CollectionName -Alias $_.Alias -Force
   }
 }
 
@@ -189,11 +311,17 @@ $ErrorActionPreference = "Stop"
 $Config = Get-Config
 
 Install-RDSWindowsFeatures
-Add-RDSessionDeployment -ConnectionBroker $Config.ConnectionBroker -SessionHosts $Config.SessionHosts -WebAccessServer $Config.WebAccessServer
+Add-RDSessionDeployment -ConnectionBroker $Config.ConnectionBroker -SessionHosts $Config.SessionHostServers -WebAccessServer $Config.WebAccessServer
 Add-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServer $Config.LicensingServer
 Add-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServer $Config.GatewayServer -GatewayExternalFqdn $Config.GatewayExternalFqdn
+
+# A SessionHost can only be part of 1 collection so remove it first
+Remove-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteAppsToKeep $Config.RemoteApps
+Remove-Collections -ConnectionBroker $Config.ConnectionBroker -CollectionsToKeep $Config.Collections
+Add-Collections -ConnectionBroker $Config.ConnectionBroker -Collections $Config.Collections
+Add-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteApps $Config.RemoteApps
 
 Remove-RDWebAccessServer -ConnectionBroker $Config.ConnectionBroker -WebAccessServerToKeep $Config.WebAccessServer
 Remove-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServerToKeep $Config.GatewayServer
 Remove-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServerToKeep $Config.LicensingServer
-Remove-SessionHosts -ConnectionBroker $Config.ConnectionBroker -SessionHostsToKeep $Config.SessionHosts
+Remove-SessionHostServer -ConnectionBroker $Config.ConnectionBroker -SessionHostServersToKeep $Config.SessionHostServers
