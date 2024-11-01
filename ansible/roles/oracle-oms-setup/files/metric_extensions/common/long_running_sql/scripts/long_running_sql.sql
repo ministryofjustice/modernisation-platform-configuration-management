0ab01da01529784058462b01582669eadd48effa
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-11-01 14:10:29 +0000

    Ignore Managed Recovery Processes

--- ansible/roles/oracle-oms-setup/files/metric_extensions/common/long_running_sql/scripts/long_running_sql.sql
+++ ansible/roles/oracle-oms-setup/files/metric_extensions/common/long_running_sql/scripts/long_running_sql.sql
@@ -19,6 +19,7 @@ where type='USER'
 and status='ACTIVE'
 and (action != 'PRF_COLL_JOB' OR action IS NULL)
 and (NOT program LIKE '%rman@%' OR program IS NULL)
+and (NOT program LIKE '%(PR__)' OR program IS NULL)
 ;
 EXIT
 EOSQL
