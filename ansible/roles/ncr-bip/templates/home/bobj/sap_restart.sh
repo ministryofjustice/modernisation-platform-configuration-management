920d92f48d5c914ac1e6ff24af110c499cec632f
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-11-14 09:59:29 +0000

    ncr: TM-693: log rotation script (#1111)
    * add archive logs script
    
    * update var
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ ansible/roles/ncr-bip/templates/home/bobj/sap_restart.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Managed in code modernisation-platform-configuration-management repo, ncr-bip role
+# This script stops SAP processes (Tomcat/CCM), archives logs, and then restarts SAP processes
+
+BOBJEDIR="{{ sap_bip_installation_directory }}/sap_bobj"
+BOBJUSER="bobj"
+CURRENTUSER=$(whoami)
+DRYRUN=${DRYRUN:-0}
+
+stop_processes() {
+  if [ -f "$BOBJEDIR"/ccm.config ]; then
+    if [[ $DRYRUN != 0 ]]; then
+      echo "DRYRUN: Stopping servers"
+    else
+      "$BOBJEDIR"/ccm.sh -stop all
+      logger -p daemon.info -t bobj "Stopped servers"
+    fi
+  fi
+  if [ -d "$BOBJEDIR"/tomcat ]; then
+    if [[ $DRYRUN != 0 ]]; then
+      echo "DRYRUN: Stopping Tomcat"
+    else
+      "$BOBJEDIR"/tomcatshutdown.sh
+      logger -p daemon.info -t bobj "Stopped Tomcat"
+    fi
+  fi
+}
+
+archive_logs() {
+  if [[ -x /home/$BOBJUSER/archive_logs.sh ]]; then
+    if [[ $DRYRUN != 0 ]]; then
+      DRYRUN=$DRYRUN /home/$BOBJUSER/archive_logs.sh
+    else
+      echo "Archiving Logs"
+      /home/$BOBJUSER/archive_logs.sh | logger -p daemon.info -t bobj
+    fi
+  fi
+}
+
+start_processes() {
+  if [ -d "$BOBJEDIR"/tomcat ]; then
+    if [[ $DRYRUN != 0 ]]; then
+      echo "DRYRUN: Starting Tomcat"
+    else
+      "$BOBJEDIR"/tomcatstartup.sh
+      logger -p daemon.info -t bobj "Started Tomcat"
+    fi
+  fi
+  if [ -f "$BOBJEDIR"/ccm.config ]; then
+    if [[ $DRYRUN != 0 ]]; then
+      echo "DRYRUN: Starting servers"
+    else
+      "$BOBJEDIR"/ccm.sh -start all
+      logger -p daemon.info -t bobj "Started servers"
+    fi
+  fi
+}
+
+if [[ "$BOBJUSER" != "$CURRENTUSER" ]]; then
+  echo "ERROR: This script must be run as $BOBJUSER"
+  [[ $DRYRUN == 0 ]] && exit 1
+fi
+
+if pgrep -u bobj java > /dev/null; then
+  stop_processes
+fi
+archive_logs
+start_processes
