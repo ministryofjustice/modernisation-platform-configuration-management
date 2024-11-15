6325bfb79a59f0f3a1f5fbb6d7c1106fcaca60c9
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-11-15 13:58:52 +0000

    unblock all downloaded files

--- powershell/Scripts/Run-GitScript.ps1
+++ powershell/Scripts/Run-GitScript.ps1
@@ -52,6 +52,7 @@ if (Test-Path -Path $GitRepo) {
 Write-Output "git clone https://github.com/${GitOrg}/${GitRepo}.git into $GitCloneDir"
 git clone -c core.longpaths=true "https://github.com/${GitOrg}/${GitRepo}.git"
 Set-Location -Path $GitRepo
+Get-ChildItem -Path "./powershell" -Filter "*.ps1" -Recurse | Unblock-File
 if ($GitBranch -ne "main") {
   git checkout "${GitBranch}"
 }
