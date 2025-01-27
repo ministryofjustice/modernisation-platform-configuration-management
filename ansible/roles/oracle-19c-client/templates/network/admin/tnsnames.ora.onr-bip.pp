333a7fae13e2a753939b5d14032af1074febfabc
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-01-27 09:44:34 +0000

    add pp pd client

---
+++ ansible/roles/oracle-19c-client/templates/network/admin/tnsnames.ora.onr-bip.pp
@@ -0,0 +1,21 @@
+# Managed by ansible modernisation-platform-configuration-management repo, oracle-19c-client role
+
+PPBOAUD =
+ (DESCRIPTION =
+  (ENABLE = broken)
+  (ADDRESS = (PROTOCOL = TCP)(HOST = pp-oasys-db-a.oasys.hmpps-preproduction.modernisation-platform.internal)(PORT = 1521))
+  (CONNECT_DATA =
+   (SERVER = DEDICATED)
+   (SERVICE_NAME = BIAUD_TAF)
+  )
+ )
+
+PPBOSYS =
+ (DESCRIPTION =
+  (ENABLE = broken)
+  (ADDRESS = (PROTOCOL = TCP)(HOST = pp-oasys-db-a.oasys.hmpps-preproduction.modernisation-platform.internal)(PORT = 1521))
+  (CONNECT_DATA =
+   (SERVER = DEDICATED)
+   (SERVICE_NAME = BISYS_TAF)
+  )
+ )
