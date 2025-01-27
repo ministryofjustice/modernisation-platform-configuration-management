4137af4f6e16bd987d266d7adc287a35dbb775b6
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-01-27 11:06:34 +0000

    TM-864: onr: build t2 bip (#1242)
    * add onr server-type files
    
    * add sap-bip-4 role
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * add pp pd client
    
    * -
    
    * -
    
    * Commit changes made by code formatters
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ ansible/roles/sap-bip-4/templates/home/bobj/tomcat_restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Managed in code modernisation-platform-configuration-management repo, ncr-bip role
+# Scheduled to run periodically via bobj cron
+. /home/bobj/.bash_profile
+/home/bobj/sap_restart.sh
