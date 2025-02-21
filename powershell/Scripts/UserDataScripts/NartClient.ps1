30a64b7f9968e6870c3d7ba7ffd1105caccf99d3
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-02-21 12:23:31 +0000

    Install session host role

--- powershell/Scripts/UserDataScripts/NartClient.ps1
+++ powershell/Scripts/UserDataScripts/NartClient.ps1
@@ -366,6 +366,18 @@ features=WebI_Rich_Client,Business_View_Manager,Report_Conversion,Universe_Desig
         }
     }
 }
+
+function Install-RDSSessionHostRole {
+    if ((Get-WindowsFeature -Name 'RDS-RD-Server').Installed) {
+        Write-Host "Remote Desktop Session Host role service already installed"
+        return
+    }
+    else {
+        Write-Host "Installing Remote Desktop Session Host role"
+        Install-WindowsFeature -Name 'RDS-RD-Server' -IncludeAllSubFeature -IncludeManagementTools
+        # May need a restart but this is covered when the machine is added to the domain
+    }
+}
 # }}} end of functions
 
 # {{{ Prep the server for installation
@@ -389,6 +401,8 @@ $Tags = Get-InstanceTags
 
 $WorkingDirectory = "C:\Software"
 $AppDirectory = "C:\App"
+
+Install-RDSSessionHostRole
 
 # {{{ join domain and rename instance to tag:'Name'
 . ../ModPlatformAD/Join-ModPlatformAD.ps1
