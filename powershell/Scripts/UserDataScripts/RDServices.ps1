679f055d09bcfbea574ba2eb31eabc1d5f158364
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-03-21 10:25:34 +0000

    add ErrorAction to other call

--- powershell/Scripts/UserDataScripts/RDServices.ps1
+++ powershell/Scripts/UserDataScripts/RDServices.ps1
@@ -241,7 +241,7 @@ $commands = {
   Remove-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteAppsToKeep $Config.RemoteApps
   Remove-Collections -ConnectionBroker $Config.ConnectionBroker -CollectionsToKeep $Config.Collections
   Add-Collections -ConnectionBroker $Config.ConnectionBroker -Collections $Config.Collections -ErrorAction SilentlyContinue
-  Add-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteApps $Config.RemoteApps
+  Add-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteApps $Config.RemoteApps -ErrorAction SilentlyContinue
 
   # # Removes servers that are NOT in the $Config block
   Remove-RDWebAccessServer -ConnectionBroker $Config.ConnectionBroker -WebAccessServerToKeep $Config.WebAccessServer
@@ -251,24 +251,6 @@ $commands = {
 }
 
 Invoke-Command -ComputerName localhost -ScriptBlock $commands -Credential $credentials -ArgumentList $Config, $PSScriptRoot -Authentication CredSSP -Verbose
-
-# Import-Module ModPlatformRemoteDesktop -Force
-# Install-RDSWindowsFeatures
-# Add-RDSessionDeployment -ConnectionBroker $Config.ConnectionBroker -SessionHosts $Config.SessionHostServers -WebAccessServer $Config.WebAccessServer
-# Add-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServer $Config.LicensingServer
-# Add-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServer $Config.GatewayServer -GatewayExternalFqdn $Config.GatewayExternalFqdn
-
-# # A SessionHost can only be part of 1 collection so remove it first
-# Remove-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteAppsToKeep $Config.RemoteApps
-# Remove-Collections -ConnectionBroker $Config.ConnectionBroker -CollectionsToKeep $Config.Collections
-# Add-Collections -ConnectionBroker $Config.ConnectionBroker -Collections $Config.Collections
-# Add-RemoteApps -ConnectionBroker $Config.ConnectionBroker -RemoteApps $Config.RemoteApps
-
-# # Removes servers that are NOT in the $Config block
-# Remove-RDWebAccessServer -ConnectionBroker $Config.ConnectionBroker -WebAccessServerToKeep $Config.WebAccessServer
-# Remove-RDGatewayServer -ConnectionBroker $Config.ConnectionBroker -GatewayServerToKeep $Config.GatewayServer
-# Remove-RDLicensingServer -ConnectionBroker $Config.ConnectionBroker -LicensingServerToKeep $Config.LicensingServer
-# Remove-SessionHostServer -ConnectionBroker $Config.ConnectionBroker -SessionHostServersToKeep $Config.SessionHostServers
 
 . ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
 
