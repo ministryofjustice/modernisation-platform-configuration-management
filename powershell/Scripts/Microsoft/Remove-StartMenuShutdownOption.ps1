aa9de99c10439dbe5b34b6eb8c18a039b284d001
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-03-28 17:09:38 +0000

    fix

--- powershell/Scripts/Microsoft/Remove-StartMenuShutdownOption.ps1
+++ powershell/Scripts/Microsoft/Remove-StartMenuShutdownOption.ps1
@@ -12,13 +12,15 @@
 
 $RegPath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start"
 if (Test-Path -Path $RegPath) {
-  $ItemProperty = Get-ItemProperty -Path $RegPath -Name HideRestart -ErrorAction SilentlyContinue
-  if ($ItemProperty -eq $null -or $ItemProperty.HideRestart -ne 1) {
-    Write-Output "Setting $RegPath\HideRestart = 1"
-    New-ItemProperty -Path $RegPath -Name HideRestart -Value 1 -PropertyType DWORD -Force | Out-Null
+  $ItemProperty = Get-ItemProperty -Path "$RegPath\HideRestart" -Name value -ErrorAction SilentlyContinue
+  if ($ItemProperty -eq $null -or $ItemProperty.value -ne 1) {
+    Write-Output "Setting $RegPath\HideRestart\value = 1"
+    New-ItemProperty -Path "$RegPath\HideRestart" -Name value -Value 1 -PropertyType DWORD -Force | Out-Null
   }
-  if ($ItemProperty -eq $null -or $ItemProperty.HideShutDown -ne 1) {
-    Write-Output "Setting $RegPath\HideShutDown = 1"
-    New-ItemProperty -Path $RegPath -Name HideShutDown -Value 1 -PropertyType DWORD -Force | Out-Null
+
+  $ItemProperty = Get-ItemProperty -Path "$RegPath\HideShutDown" -Name value -ErrorAction SilentlyContinue
+  if ($ItemProperty -eq $null -or $ItemProperty.value -ne 1) {
+    Write-Output "Setting $RegPath\HideShutDown\value = 1"
+    New-ItemProperty -Path "$RegPath\HideShutDown" -Name value -Value 1 -PropertyType DWORD -Force | Out-Null
   }
 }
