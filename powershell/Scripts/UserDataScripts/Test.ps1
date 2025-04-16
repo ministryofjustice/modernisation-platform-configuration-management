035df4447e57593f59ba53e11746c8996ee04517
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-04-16 10:56:53 +0100

    reboot at time of script execution

--- powershell/Scripts/UserDataScripts/Test.ps1
+++ powershell/Scripts/UserDataScripts/Test.ps1
@@ -27,11 +27,15 @@ $OverallExitCode = 0
 foreach ($Script in $Scripts) {
   Write-Output "START $Script"
   . $Script
+  if ($LASTEXITCODE -eq 3010) {
+    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Exit 3010  # Exit immediately to allow reboot
+  }
   if ($LASTEXITCODE -ne 0) {
     $OverallExitCode = $LASTEXITCODE
     Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
   } else {
-    Write-Output "END   $Script"
+    Write-Output "END $Script"
   }
   Set-Location $ScriptDir
 }
