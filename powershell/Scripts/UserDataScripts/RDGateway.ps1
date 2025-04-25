084689b58d65197b80ee05047ebdd6e03baaf3ef
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-04-25 14:53:35 +0100

    -

--- powershell/Scripts/UserDataScripts/RDGateway.ps1
+++ powershell/Scripts/UserDataScripts/RDGateway.ps1
@@ -1,39 +1,32 @@
+$Scripts = @(
+    "../Common/Set-LocalFirewallOff.ps1",
+    "../Common/Set-IPv4Preferred.ps1",
+    "../Common/Set-GMTTimezone.ps1",
+    "../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname keep-existing",
+    "../Microsoft/Add-DnsSuffixSearchList.ps1",
+    "../Microsoft/Remove-EdgeFirstRunExperience.ps1",
+    "../Microsoft/Remove-StartMenuShutdownOption.ps1",
+    "../ModPlatformRemoteDesktop/Add-ModPlatformRDGateway.ps1 -EnableSSLBridging $false",
+    "../ModPlatformAD/Move-ModPlatformADComputer.ps1",
+    "../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1"
+)
+
 $ErrorActionPreference = "Stop"
-
-. ../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname "keep-existing"
-
-if ($LASTEXITCODE -ne 0) {
-   Exit $LASTEXITCODE
+$ScriptDir = Get-Location
+$OverallExitCode = 0
+foreach ($Script in $Scripts) {
+  Write-Output "START $Script"
+  . $Script
+  if ($LASTEXITCODE -eq 3010) {
+    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Exit 3010  # Exit immediately to allow reboot
+  }
+  if ($LASTEXITCODE -ne 0) {
+    $OverallExitCode = $LASTEXITCODE
+    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+  } else {
+    Write-Output "END $Script"
+  }
+  Set-Location $ScriptDir
 }
-
-$DomainNameNetbios = $ADConfig.DomainNameNetbios
-
-$CAP = @{
-  "Name" = "default"
-  "AuthMethod" = 1
-  "Status" = 1
-  "IdleTimeout" = 120
-  "SessionTimeout" = 480
-  "SessionTimeoutAction" = 0
-  "UserGroups" = "Domain Users@${DomainNameNetbios}"
-}
-$RAP = @{
-  "Name" = "default"
-  "ComputerGroupType" = 2
-  "UserGroups" = "Domain Users@${DomainNameNetbios}"
-}
-
-Import-Module ModPlatformRemoteDesktop -Force
-
-$Feature = Get-WindowsFeature -Name RDS-Gateway
-Add-ModPlatformRDGateway -EnableSSLBridging $false
-Set-ModPlatformRDGatewayCAP @CAP
-Set-ModPlatformRDGatewayRAP @RAP
-
-. ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
-
-if (-not $Feature.Installed) {
-  Exit 3010 # triggers reboot on first install otherwise doesn't work
-}
-
-Exit $LASTEXITCODE
+Exit $OverallExitCode
