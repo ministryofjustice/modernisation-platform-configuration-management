c61eb49e9248cf6cc3c21ca3da733b444e5814e1
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-04-28 15:20:58 +0100

    support arguments

--- powershell/Scripts/UserDataScripts/RDGateway.ps1
+++ powershell/Scripts/UserDataScripts/RDGateway.ps1
@@ -2,11 +2,11 @@ $Scripts = @(
     "../Common/Set-LocalFirewallOff.ps1",
     "../Common/Set-IPv4Preferred.ps1",
     "../Common/Set-GMTTimezone.ps1",
-    "../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname keep-existing",
+    @("../ModPlatformAD/Join-ModPlatformAD.ps1", @{NewHostname = "keep-existing"}),
     "../Microsoft/Add-DnsSuffixSearchList.ps1",
     "../Microsoft/Remove-EdgeFirstRunExperience.ps1",
     "../Microsoft/Remove-StartMenuShutdownOption.ps1",
-    "../ModPlatformRemoteDesktop/Add-ModPlatformRDGateway.ps1 -EnableSSLBridging $false",
+    @("../ModPlatformRemoteDesktop/Add-ModPlatformRDGateway.ps1", @{EnableSSLBridging = $false}),
     "../ModPlatformAD/Move-ModPlatformADComputer.ps1",
     "../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1"
 )
@@ -15,17 +15,25 @@ $ErrorActionPreference = "Stop"
 $ScriptDir = Get-Location
 $OverallExitCode = 0
 foreach ($Script in $Scripts) {
-  Write-Output "START $Script"
-  . $Script
+  if ($Script -is [array]) {
+    $ScriptCmd = $Script[0]
+    $ScriptArg = $Script[1]
+    Write-Output "START $ScriptCmd @ScriptArg"
+    . $ScriptCmd @ScriptArg
+  } else {
+    $ScriptCmd = $Script
+    Write-Output "START $ScriptCmd"
+    . $ScriptCmd
+  }
   if ($LASTEXITCODE -eq 3010) {
     Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
     Exit 3010  # Exit immediately to allow reboot
   }
   if ($LASTEXITCODE -ne 0) {
     $OverallExitCode = $LASTEXITCODE
-    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+    Write-Output "ERROR $ScriptCmd ExitCode=$LASTEXITCODE"
   } else {
-    Write-Output "END $Script"
+    Write-Output "END $ScriptCmd"
   }
   Set-Location $ScriptDir
 }
