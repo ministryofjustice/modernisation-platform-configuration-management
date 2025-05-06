4b1130425a195157a825d4a422abbbb8f278450c
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-05-06 11:30:41 +0100

    create test file

---
+++ powershell/Scripts/UserDataScripts/RDPTest.ps1
@@ -0,0 +1,30 @@
+$Scripts = @(
+    "../Common/Set-LocalFirewallOff.ps1",
+    "../Common/Set-IPv4Preferred.ps1",
+    "../Common/Set-TimezoneGMT.ps1",
+    "../Common/Set-PermanentPSModulePath.ps1",
+    "../Common/Install-Putty.ps1",
+    "../Common/Install-WinSCP.ps1",
+    "../ModPlatformAD/Join-ModPlatformAD.ps1",
+    "../ModPlatformAD/Move-ModPlatformADComputer.ps1"
+)
+
+$ErrorActionPreference = "Stop"
+$ScriptDir = Get-Location
+$OverallExitCode = 0
+foreach ($Script in $Scripts) {
+  Write-Output "START $Script"
+  . $Script
+  if ($LASTEXITCODE -eq 3010) {
+    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Exit 3010  # Exit immediately to allow reboot
+  }
+  if ($LASTEXITCODE -ne 0) {
+    $OverallExitCode = $LASTEXITCODE
+    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+  } else {
+    Write-Output "END $Script"
+  }
+  Set-Location $ScriptDir
+}
+Exit $OverallExitCode
