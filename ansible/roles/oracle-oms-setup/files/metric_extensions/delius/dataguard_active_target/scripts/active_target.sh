06930c579b906de4749d98acc00db2e6ef99e361
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-05-06 10:48:10 +0100

    Detect if FSFO Active Target on 1st Standby
    1st Standby is the preferred target for FSFO as the 2nd standby is used for Active Data Guard.
    This Metric used to perform the check if the Observer was running, but in MP we need instead to check if FSFO is enabled and do not raise an error if it is disabled.

--- ansible/roles/oracle-oms-setup/files/metric_extensions/delius/dataguard_active_target/scripts/active_target.sh
+++ ansible/roles/oracle-oms-setup/files/metric_extensions/delius/dataguard_active_target/scripts/active_target.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Check Active Target is the 1st Standby Database
+# Check Active Target is the 1st Standby Database is FSFO is enabled
 
 . ~/.bash_profile
 
+FSFO_ENABLED=$( echo "show configuration;" | dgmgrl / | grep -c "Fast-Start Failover: Enabled in Zero Data Loss Mode" )
 ACTIVE_TARGET=$( echo "show configuration;" | dgmgrl / | grep "(*) Physical standby database" | awk '{print substr($1,length($1)-1)}')
-# Check if target is not being Observed
-NO_OBSERVER=$( echo "show configuration;" | dgmgrl / | grep -c "ORA-16820" )
 
-if [[ "${ACTIVE_TARGET}" == "s1" && ${NO_OBSERVER} -eq 0 ]];
+if [[ "${ACTIVE_TARGET}" == "s1" && ${FSFO_ENABLED} -eq 1 ]];
 then
-   echo "YES"
+   # OK is the Active Target database is the 1st Standby and FSFO is Enabled
+   echo "OK"
 else
-   echo "NO"
+   if [[ ${FSFO_ENABLED} -eq 0 ]];
+   then
+          # Also OK if FSFO is not enabled at all
+	   echo "OK"
+   else
+           # Otherwise raise an error
+   	   echo "NO"
+   fi
 fi
