6189d9314d740478855c98438be0c5bc3a600a24
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-06-09 09:22:04 +0100

    add contents

---
+++ powershell/Scripts/UserDataScripts/MISDis.ps1
@@ -0,0 +1,23 @@
+$Scripts = @(
+    "../Common/Set-TimezoneGMT.ps1"
+)
+
+$ErrorActionPreference = "Stop"
+$ScriptDir = Get-Location
+$OverallExitCode = 0
+foreach ($Script in $Scripts) {
+  Write-Output "START $Script"
+  . $Script
+  if ($LASTEXITCODE -eq 3010) {
+    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Exit 3010  # Exit immediately to allow reboot
+  }
+  if ($LASTEXITCODE -ne 0) {
+    $OverallExitCode = $LASTEXITCODE
+    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+  } else {
+    Write-Output "END $Script"
+  }
+  Set-Location $ScriptDir
+}
+Exit $OverallExitCode
