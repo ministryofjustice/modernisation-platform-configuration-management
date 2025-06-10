b2f5f3494e19ed0d79980b0c824a675f2c66b4c0
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-06-10 11:48:21 +0100

    add winrar install

--- powershell/Scripts/UserDataScripts/BIPTemp.ps1
+++ powershell/Scripts/UserDataScripts/BIPTemp.ps1
@@ -2,25 +2,10 @@ $Scripts = @(
     "../Common/Set-LocalFirewallOff.ps1",
     "../Common/Set-IPv4Preferred.ps1",
     "../Common/Set-TimezoneGMT.ps1",
+    "../Common/Install-WinRAR.ps1",
     "../ModPlatformAD/Join-ModPlatformAD.ps1",
-    # "../ModPlatformAD/Move-ModPlatformADComputer.ps1",
-    # "../Oracle/Install-Java6.ps1",
-    # "../Oracle/Install-Java8.ps1",
-    # "../Oracle/Install-JavaDeployment.ps1",
-    # "../Oracle/Install-SQLDeveloper.ps1",
-    # "../Oracle/Remove-JavaUpdateCheck.ps1",
-    # "../Microsoft/Add-DnsSuffixSearchList.ps1",
-    # "../Microsoft/Add-EdgeInternetExplorerIntegration.ps1",
-    # "../Microsoft/Add-EdgePopupsAllowedForUrls.ps1",
-    # "../Microsoft/Add-StartMenuShortcuts.ps1",
-    # "../Microsoft/Install-ADRemoteTools.ps1",
     "../Microsoft/Remove-EdgeFirstRunExperience.ps1",
-    # "../Microsoft/Remove-StartMenuShutdownOption.ps1",
-    # "../LibreOffice/Install-LibreOffice.ps1",
     "../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1",
-    # "../Oracle/Install-Oracle19cClient.ps1",
-    # "../Oracle/Set-TnsOraFile.ps1",
-    # "../SAP/Install-BIPWindowsClient43.ps1"
     "../SAP/Get-WindowsBIPSoftware.ps1"
 )
 
