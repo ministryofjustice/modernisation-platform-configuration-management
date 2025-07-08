ae0238ae6390be3452622015680aac9850d781dd
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-07-08 12:30:45 +0100

    dataservices silent install and config changes

--- powershell/Configs/MISDis/misdis_config.ps1
+++ powershell/Configs/MISDis/misdis_config.ps1
@@ -5,15 +5,17 @@ $GlobalConfig = @{
         "WindowsClientS3Folder"      = "hmpps/mis"
         "IPSS3File"                  = "IPS4304P_900-70002778.EXE" # Information Platform Services 4.3 SP9 Patch 7
         "DataServicesS3File"         = "DS4303P_4-80007397.EXE" # Data Services 4.3 SP3 Patch 4
-        "LINK_DIR"                   = "D:\SAP BusinessObjects\Data Services"
-        "BIP_INSTALL_DIR"            = "D:\SAP BusinessObjects\SAP BusinessObjects Enterprise XI 4.0"
+        "LINK_DIR"                   = "D:\BusinessObjects\Data Services"
+        "BIP_INSTALL_DIR"            = "D:\BusinessObjects\SAP BusinessObjects Enterprise XI 4.0"
         "AppDirectory"               = "D:\App"
+        "dscommondir"                = "D:\DSCommon"
     }
     "delius-mis-development" = @{
         "sysDbName"              = "DMDDSD"
         "audDbName"              = "DMDDSD"
-        "cmsPrimaryNode"         = "delius-mis-dev-dfi-1"
+        "cmsPrimaryNode"         = "ndmis-dev-dfi-1"
         "serviceUser"            = "SVC_DIS_NDL" # Only used for dataservices install
+        "Domain"                 = "delius-mis-dev"
     }
     "delius-mis-preproduction" = @{
     }
