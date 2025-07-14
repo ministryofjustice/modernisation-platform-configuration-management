8613fb2ce4de23cab7f73bb25f3c8c627dea17eb
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-07-14 11:26:42 +0100

    change comments

--- powershell/Scripts/UserDataScripts/MISDis.ps1
+++ powershell/Scripts/UserDataScripts/MISDis.ps1
@@ -1,20 +1,16 @@
 $Scripts = @(
-    "../Common/Set-TimezoneGMT.ps1", # working
-    "../Common/Set-LoginText.ps1", # working
-    "../Oracle/Install-Oracle19cClient.ps1", # working
-    "../Oracle/Set-TnsOraFileMISDis.ps1", # working
-    "../Oracle/Install-SQLDeveloper.ps1", # working
-    "../MISDis/Move-ComputerDeliusInternalAD.ps1" # working
-    "../Oracle/Test-DbCredentials-MISDIS-DEV-ONLY.ps1", # <- testing
-    "../Oracle/Install-IPS-MISDIS-DEV-ONLY.ps1", #, <- testing
-    "../Oracle/Install-DataServices-MISDIS-DEV-ONLY.ps1" # <- testing
-    <#
-    New-SharedDriveShortcut -Config $Config <- required?? Possibly - TBD
-    New-ServerRebootSchedule -Config $Config <- required?? No - also what is this?
-    #>
+  '../Common/Set-TimezoneGMT.ps1',
+  '../Common/Set-LoginText.ps1',
+  '../Oracle/Install-Oracle19cClient.ps1',
+  '../Oracle/Set-TnsOraFileMISDis.ps1',
+  '../Oracle/Install-SQLDeveloper.ps1',
+  '../MISDis/Move-ComputerDeliusInternalAD.ps1',
+  '../Oracle/Test-DbCredentials-MISDIS-DEV-ONLY.ps1',
+  '../Oracle/Install-IPS-MISDIS-DEV-ONLY.ps1', # <- fix as part of TM-1414
+  '../Oracle/Install-DataServices-MISDIS-DEV-ONLY.ps1' # <- fix as part of TM-1414
 )
 
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = 'Stop'
 $ScriptDir = Get-Location
 $OverallExitCode = 0
 foreach ($Script in $Scripts) {
@@ -22,14 +18,15 @@ foreach ($Script in $Scripts) {
   . $Script
   if ($LASTEXITCODE -eq 3010) {
     Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
-    Exit 3010  # Exit immediately to allow reboot
+    exit 3010  # Exit immediately to allow reboot
   }
   if ($LASTEXITCODE -ne 0) {
     $OverallExitCode = $LASTEXITCODE
     Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
-  } else {
+  }
+  else {
     Write-Output "END $Script"
   }
   Set-Location $ScriptDir
 }
-Exit $OverallExitCode
+exit $OverallExitCode
