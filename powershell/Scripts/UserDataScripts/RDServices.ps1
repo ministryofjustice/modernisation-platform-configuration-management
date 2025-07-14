6169f044079537beed8e63a33a16ad4024bac2d6
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-14 10:54:31 +0100

    fix permanent path

--- powershell/Scripts/UserDataScripts/RDServices.ps1
+++ powershell/Scripts/UserDataScripts/RDServices.ps1
@@ -138,33 +138,6 @@ function Get-Config {
   Return $GlobalConfig[$NameTag]
 }
 
-
-function Add-PermanentPSModulePath {
-  param(
-    [Parameter(Mandatory = $true)]
-    [string]$NewPath
-  )
-  # IMPORTANT: This will currently only make these visible when running as ADMINISTRATOR
-
-  # Get current Machine PSModulePath from the registry
-  $regKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
-  $currentValue = (Get-ItemProperty -Path $regKey -Name PSModulePath).PSModulePath
-
-  # Check if the path already exists
-  if ($currentValue -split ';' -notcontains $NewPath) {
-    # Add the new path
-    $newValue = $currentValue + ";" + $NewPath
-
-    # Update the registry
-    Set-ItemProperty -Path $regKey -Name PSModulePath -Value $newValue
-
-    Write-Host "Added $NewPath to system PSModulePath. Changes will take effect after restart or refreshing environment variables."
-  }
-  else {
-    Write-Host "$NewPath is already in PSModulePath"
-  }
-}
-
 function Move-ModPlatformADComputer {
   [CmdletBinding()]
   param (
@@ -202,14 +175,6 @@ function Move-ModPlatformADComputer {
 $ErrorActionPreference = "Stop"
 
 $Config = Get-Config
-
-# Add modules permanently to PSModulePath
-$ModulesPath = Join-Path $PSScriptRoot "..\..\Modules"
-Add-PermanentPSModulePath -NewPath $ModulesPath
-# Add to system environment (persistent)
-[Environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath + ";" + $ModulesPath, "Machine")
-# Also add to current session
-$env:PSModulePath = $env:PSModulePath + ";" + $ModulesPath
 
 # Change name and Join the domain
 . ../ModPlatformAD/Join-ModPlatformAD.ps1
