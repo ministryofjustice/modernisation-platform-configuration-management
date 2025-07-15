3724258d991ea2d6162f8a3791fb9ef27145efd3
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-15 16:59:41 +0100

    -

---
+++ powershell/Scripts/Common/Install-Choco-Package.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Install a package via Chocolatey 
+
+.EXAMPLE
+    Install-Choco-Package.ps1 -Package putty
+#>
+
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory = $true)][string]$Package
+)
+
+# Check whether Chocolatey powershell module is installed
+# This just makes it easier to get packages, versions and so on
+# This isn't installing chocolatey
+if (Get-Module -Name chocolatey) {
+    Write-Verbose "Chocolatey PowerShell module installed, importing module"
+    Import-Module -Name chocolatey -Force
+} else {
+    Write-Output "Chocolatey PowerShell module not installed, installing and importing"
+    Install-Module -Name chocolatey -Force
+    Import-Module -Name chocolatey -Force
+}
+
+$ChocolateyPackage = Get-ChocolateyPackage -Name $Package
+if ($ChocolateyPackage) {
+    Write-Verbose ("$Package already installed, version: " + $ChocolateyPackage.version)
+} elseif ($WhatIfPreference) {
+    choco install $Package -y --no-progress --whatif
+} else {
+    choco install $Package -y --no-progress
+}
--- powershell/Scripts/Common/Set-IPv4Preferred.ps1
+++ powershell/Scripts/Common/Set-IPv4Preferred.ps1
@@ -8,12 +8,12 @@ if (Test-Path $registryPath) {
     $property = Get-ItemProperty -Path $registryPath -Name $propertyName -ErrorAction SilentlyContinue
     
     if ($property -and $property.$propertyName -eq $desiredValue) {
-        Write-Host "Registry already configured to prefer IPv4 over IPv6."
+        Write-Verbose "Registry already configured to prefer IPv4 over IPv6."
     } else {
         # Set the property to prefer IPv4 over IPv6
+        Write-Output "Updating registry to prefer IPv4 over IPv6. A system restart is required for changes to take effect."
         Set-ItemProperty -Path $registryPath -Name $propertyName -Value $desiredValue -Type DWord
-        Write-Host "Registry updated to prefer IPv4 over IPv6. A system restart is required for changes to take effect."
     }
 } else {
-    Write-Host "Registry path does not exist: $registryPath"
+    Write-Output "Cannot set IPv4 preference as Registry path does not exist: $registryPath"
 }
