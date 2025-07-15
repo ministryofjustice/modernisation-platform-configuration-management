940ef982d5d5b118aea24edc39d44268d7a2f565
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-15 16:14:09 +0100

    Update Common scripts for better use of whatif and verbose

--- powershell/Scripts/Common/Set-LocalFirewallOff.ps1
+++ powershell/Scripts/Common/Set-LocalFirewallOff.ps1
@@ -1,2 +1,11 @@
 # Turn off the firewall as this will possibly interfere with Sia Node creation or other installations
-Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+$Profiles = @("Domain", "Public", "Private")
+foreach ($Profile in $Profiles) {
+  $NetFirewallProfile = Get-NetFirewallProfile -Profile $Profile
+  if ($NetFirewallProfile.Enabled -ne $false) {
+    Write-Output "Disabling firewall for profile $Profile"
+    Set-NetFirewallProfile -Profile $Profile -Enabled False
+  } else {
+    Write-Verbose "Firewall already disabled for profile $Profile"
+  }
+}
