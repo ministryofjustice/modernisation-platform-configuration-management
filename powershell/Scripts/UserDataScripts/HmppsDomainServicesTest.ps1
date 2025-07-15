7a3dd6cc213972ac679bea40038f82d7f3f10f84
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-15 14:36:11 +0100

    test

--- powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
+++ powershell/Scripts/UserDataScripts/HmppsDomainServicesTest.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = "Stop"
 
-. ../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname "keep-existing"
+#. ../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname "keep-existing"
 
-if ($LASTEXITCODE -ne 0) {
-   Exit $LASTEXITCODE
-}
+#if ($LASTEXITCODE -ne 0) {
+#   Exit $LASTEXITCODE
+#}
 
-. ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
-
-Exit $LASTEXITCODE
+#. ../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1
+#
+#Exit $LASTEXITCODE
