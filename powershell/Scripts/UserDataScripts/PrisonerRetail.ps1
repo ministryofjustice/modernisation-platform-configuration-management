9dcdaef2efd142a25e8bf5779a151f54a73a4fc3
wullub <13420301+wullub@users.noreply.github.com>
2025-07-17 14:02:49 +0100

    prisoner retail ec2 poc

--- powershell/Scripts/UserDataScripts/PrisonerRetail.ps1
+++ powershell/Scripts/UserDataScripts/PrisonerRetail.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-. ../ModPlatformAD/Join-ModPlatformAD.ps1
+. ../ModPlatformAD/Join-ModPlatformAD.ps1 -NewHostname "keep-existing"
 
 if ($LASTEXITCODE -ne 0) {
    Exit $LASTEXITCODE
