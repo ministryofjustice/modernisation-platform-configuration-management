a9993bc26861b7d07941e4e4ae5880394a7e3698
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-17 13:25:26 +0100

    -

--- powershell/Scripts/Common/Install-Choco-Package.ps1
+++ powershell/Scripts/Common/Install-Choco-Package.ps1
@@ -14,16 +14,22 @@ param (
 # Check whether Chocolatey powershell module is installed
 # This just makes it easier to get packages, versions and so on
 # This isn't installing chocolatey
+$ChocolateyPackage = $null
 if (Get-Module -Name chocolatey) {
     Write-Verbose "Chocolatey PowerShell module installed, importing module"
     Import-Module -Name chocolatey -Force
+    $ChocolateyPackage = Get-ChocolateyPackage -Name $Package
 } else {
     Write-Output "Chocolatey PowerShell module not installed, installing and importing"
-    Install-Module -Name chocolatey -Force -WhatIf $false
-    Import-Module -Name chocolatey -Force
+    Install-Module -Name chocolatey -Force
+    if ($WhatIfPreference) {
+        Write-Output "What-If: Import-Module -Name chocolatey -Force"
+    } else {
+        Import-Module -Name chocolatey -Force
+        $ChocolateyPackage = Get-ChocolateyPackage -Name $Package
+    }
 }
 
-$ChocolateyPackage = Get-ChocolateyPackage -Name $Package
 if ($ChocolateyPackage) {
     Write-Verbose ("$Package already installed, version: " + $ChocolateyPackage.version)
 } elseif ($WhatIfPreference) {
