9a0bbd86f9c5240b82978da8b5754d8993ded3c1
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-07-29 16:56:15 +0100

    change splatting use

--- powershell/Scripts/UserDataScripts/EscallationTest.ps1
+++ powershell/Scripts/UserDataScripts/EscallationTest.ps1
@@ -8,7 +8,8 @@ $ScriptDir = Get-Location
 $OverallExitCode = 0
 foreach ($Script in $Scripts) {
     Write-Output "START $($Script.Path)"
-    & $Script.Path @Script.Args
+    $ScriptArgs = $Script.Args
+    & $Script.Path @ScriptArgs
     if ($LASTEXITCODE -eq 3010) {
         Write-Output "REBOOT REQUIRED after $($Script.Path) - exiting with code 3010"
         exit 3010  # Exit immediately to allow reboot
