6c9f3509e358817776feb536327bea409cda012e
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-31 16:13:50 +0100

    TM-1440: planetfm: RDWeb preprod tidyup (#1506)
    * Remove old 2022 test
    
    * add 2022 host to same collection as the 2012
    
    * remove 20
    
    * fix calc

--- powershell/Scripts/ModPlatformRemoteDesktop/Add-ModPlatformRDServices.ps1
+++ powershell/Scripts/ModPlatformRemoteDesktop/Add-ModPlatformRDServices.ps1
@@ -41,7 +41,7 @@ $GlobalConfig = @{
     "LicensingServer"     = "AD-HMPP-RDLIC.AZURE.HMPP.ROOT"
     "GatewayServer"       = "$env:computername.AZURE.HMPP.ROOT"
     "GatewayExternalFqdn" = "rdgateway1.preproduction.hmpps-domain.service.justice.gov.uk"
-    "SessionHostServers"  = @("PP-CAFM-A-11-A.AZURE.HMPP.ROOT", "PP-CAFM-A-20-A.AZURE.HMPP.ROOT", "PP-JUMP2022-1.AZURE.HMPP.ROOT")
+    "SessionHostServers"  = @("PP-CAFM-A-11-A.AZURE.HMPP.ROOT", "PP-JUMP2022-1.AZURE.HMPP.ROOT")
     "WebAccessServer"     = "$env:computername.AZURE.HMPP.ROOT"
     "svcRdsSecretsVault"  = "/microsoft/AD/azure.hmpp.root/shared-passwords"
     "domain"              = "HMPP"
@@ -51,13 +51,6 @@ $GlobalConfig = @{
         "Configuration" = @{
           "CollectionDescription" = "CAFM-RDP PreProd Modernisation Platform"
           "UserGroup"             = @("HMPP\PROD_CAFM_SQL_USERS")
-        }
-      }
-      "CAFM-RDP PreProd 2022" = @{
-        "SessionHosts"  = @("PP-CAFM-A-20-A.AZURE.HMPP.ROOT")
-        "Configuration" = @{
-          "CollectionDescription" = "CAFM-RDP PreProd 2022 Modernisation Platform"
-          "UserGroup"             = @("HMPP\drobinson", "HMPP\drobinson2", "HMPP\drobinson3", "HMPP\DQS06X")
         }
       }
       "pp-jump2022-1" = @{
@@ -72,21 +65,11 @@ $GlobalConfig = @{
       "calc"             = @{
         "CollectionName" = "CAFM-RDP PreProd"
         "DisplayName"    = "Calculator"
-        "FilePath"       = 'C:\Windows\system32\calc.exe'
+        "FilePath"       = 'C:\Windows\system32\win32calc.exe'
       }
       "PlanetEnterprise" = @{
         "CollectionName" = "CAFM-RDP PreProd"
         "DisplayName"    = "Qube Planet"
-        "FilePath"       = 'C:\Program Files (x86)\Qube\Planet FM Enterprise\Programs\PlanetEnterprise.exe'
-      }
-      "calc2022"             = @{
-        "CollectionName" = "CAFM-RDP PreProd 2022"
-        "DisplayName"    = "Calculator 2022"
-        "FilePath"       = 'C:\Windows\system32\win32calc.exe'
-      }
-      "PlanetEnterprise2022" = @{
-        "CollectionName" = "CAFM-RDP PreProd 2022"
-        "DisplayName"    = "Qube Planet 2022"
         "FilePath"       = 'C:\Program Files (x86)\Qube\Planet FM Enterprise\Programs\PlanetEnterprise.exe'
       }
     }
