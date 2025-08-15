6652580dadbae523e02b41fc4aa1c9fd76de44cf
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-08-15 09:45:15 +0100

    add option to invoke-userdata script

--- powershell/Scripts/Invoke-UserDataScript.ps1
+++ powershell/Scripts/Invoke-UserDataScript.ps1
@@ -3,13 +3,17 @@
     Get server-type tag and run associated UserDataScript
 #>
 
+param (
+  [string]$ScriptNameExtension = ""
+)
+
 $ErrorActionPreference = "Stop"
 $Token = Invoke-RestMethod -TimeoutSec 10 -Headers @{"X-aws-ec2-metadata-token-ttl-seconds"=3600} -Method PUT -Uri http://169.254.169.254/latest/api/token
 $InstanceId = Invoke-RestMethod -TimeoutSec 10 -Headers @{"X-aws-ec2-metadata-token" = $Token} -Method GET -Uri http://169.254.169.254/latest/meta-data/instance-id
 $TagsRaw = aws ec2 describe-tags --filters "Name=resource-id,Values=$InstanceId"
 $Tags = "$TagsRaw" | ConvertFrom-Json
 $ServerTypeTag = ($Tags.Tags | Where-Object  {$_.Key -eq "server-type"}).Value
-$UserDataScript = "${ServerTypeTag}.ps1"
+$UserDataScript = "${ServerTypeTag}${ScriptNameExtension}.ps1"
 
 Set-Location -Path "UserDataScripts"
 if (-not $ServerTypeTag) {
