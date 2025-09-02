468b35a49b69db53af4e3d2ca63a481dac07ad39
dave-belton <david.belton@justice.gov.uk>
2025-09-02 14:47:17 +0100

    Update install.sh
    Add trustedCerts arg with the Amazon cert when installing osbws so that it is added to the wallet

--- ansible/roles/oracle-secure-backup/templates/install.sh
+++ ansible/roles/oracle-secure-backup/templates/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:"{{ osw_stage_dir }}/jdk1.7.0_80/bin"
-java -jar "{{ osw_stage_dir }}/osbws_install.jar" -ARGFILE "{{ osw_stage_dir }}/{{ osbws_config.name }}_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" -useHttps -no-check-certificate | logger -p local3.info -t java
+java -jar "{{ osw_stage_dir }}/osbws_install.jar" -ARGFILE "{{ osw_stage_dir }}/{{ osbws_config.name }}_argfile" -IAMRole "{{ ansible_ec2_iam_instance_profile_role }}" -useHttps -no-check-certificate -trustedCerts "{{ osw_stage_dir }}/AmazonRootCA1.pem" | logger -p local3.info -t java
