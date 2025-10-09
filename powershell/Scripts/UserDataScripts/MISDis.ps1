98fe5c21fd42e0a2d3fdbf0759ce7eaa388df5e3
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-10-09 12:18:19 +0100

    call scripts with parameters without breaking things

--- powershell/Scripts/UserDataScripts/MISDis.ps1
+++ powershell/Scripts/UserDataScripts/MISDis.ps1
@@ -1,35 +1,43 @@
 $Scripts = @(
-  '../Common/Set-TimezoneGMT.ps1',
-  '../Common/Set-LoginText.ps1',
-  '../Common/Set-WindowsFirewallPortOpenInbound.ps1 -Port 8080', # Required for LB connections
-  '../Common/Set-LocalFirewallOff.ps1', # Required for BODS services Automatic (delayed start) to succeed
-  '../Oracle/Install-Oracle19cClient.ps1',
-  '../Oracle/Set-TnsOraFileMISDis.ps1',
-  '../Oracle/Install-SQLDeveloper.ps1',
-  '../MISDis/Move-ComputerDeliusInternalAD.ps1',
-  '../Common/Test-InstallationEscalation.ps1',
-  '../Common/Install-WinRAR.ps1',
-  '../Oracle/Test-DbCredentials.ps1',
-  '../Oracle/Install-IPS.ps1',
-  '../Oracle/Install-DataServices.ps1' 
+  @{ Path = '../Common/Set-TimezoneGMT.ps1'; Args = @{} },
+  @{ Path = '../Common/Set-LoginText.ps1'; Args = @{} },
+  @{ Path = '../Common/Set-WindowsFirewallPortOpenInbound.ps1'; Args = @{ Port = 8080 } }, # Required for LB connections
+  @{ Path = '../Common/Set-LocalFirewallOff.ps1'; Args = @{} }, # Required for BODS services Automatic (delayed start) to succeed
+  @{ Path = '../Oracle/Install-Oracle19cClient.ps1'; Args = @{} },
+  @{ Path = '../Oracle/Set-TnsOraFileMISDis.ps1'; Args = @{} },
+  @{ Path = '../Oracle/Install-SQLDeveloper.ps1'; Args = @{} },
+  @{ Path = '../MISDis/Move-ComputerDeliusInternalAD.ps1'; Args = @{} },
+  @{ Path = '../Common/Test-InstallationEscalation.ps1'; Args = @{} },
+  @{ Path = '../Common/Install-WinRAR.ps1'; Args = @{} },
+  @{ Path = '../Oracle/Test-DbCredentials.ps1'; Args = @{} },
+  @{ Path = '../Oracle/Install-IPS.ps1'; Args = @{} },
+  @{ Path = '../Oracle/Install-DataServices.ps1'; Args = @{} },
+  @{ Path = '../Common/Set-ServiceAutoDelayedStart.ps1'; Args = @{ Services = @('SAP Data Services', 'Server Intelligence Agent*', 'Apache Tomcat*') } }
 )
 
 $ErrorActionPreference = 'Stop'
 $ScriptDir = Get-Location
 $OverallExitCode = 0
 foreach ($Script in $Scripts) {
-  Write-Output "START $Script"
-  . $Script
+  $scriptPath = $Script.Path
+  $scriptArgs = $Script.Args
+  Write-Output "START $scriptPath"
+  if ($scriptArgs.Count -gt 0) {
+    & $scriptPath @scriptArgs
+  }
+  else {
+    . $scriptPath
+  }
   if ($LASTEXITCODE -eq 3010) {
-    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Write-Output "REBOOT REQUIRED after $scriptPath - exiting with code 3010"
     exit 3010  # Exit immediately to allow reboot
   }
   if ($LASTEXITCODE -ne 0) {
     $OverallExitCode = $LASTEXITCODE
-    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+    Write-Output "ERROR $scriptPath ExitCode=$LASTEXITCODE"
   }
   else {
-    Write-Output "END $Script"
+    Write-Output "END $scriptPath"
   }
   Set-Location $ScriptDir
 }
