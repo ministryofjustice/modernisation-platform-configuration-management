4c7c1eabc3563582a64a47f52f21c362124d9616
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-12-18 15:52:58 +0000

    -

---
+++ powershell/Scripts/ModPlatformAD/Add-ModPlatformADUsers.ps1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+    Add or update AD Users
+
+.DESCRIPTION
+    Get list of AD Users for the given domain and add or update them.
+    Includes adding to relevant AD Group membership.
+    EC2 requires permissions to get tags and the aws cli.
+
+.PARAMETER DomainNameFQDN
+    Optionally specify the FQDN of the domain name to join, otherwise derived from tag
+
+.EXAMPLE
+    Add-ModPlatformADUsers
+#>
+
+[CmdletBinding()]
+param (
+  [string]$DomainNameFQDN
+)
+
+Import-Module ModPlatformAD -Force
+
+$ErrorActionPreference = "Stop"
+
+$ADConfig     = Get-ModPlatformADConfig -DomainNameFQDN $DomainNameFQDN
+$ADCredential = Get-ModPlatformADAdminCredential -ModPlatformADConfig $ADConfig
+$ADUsers      = Get-ModPlatformADUserConfig $ADConfig.DomainNameFQDN
+Get-ModPlatformADUserCredentials $ADUsers
+Add-ModPlatformADUsers $ADUsers $ADCredential
