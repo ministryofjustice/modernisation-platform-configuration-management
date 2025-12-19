207ed0f10c3d784a6b758f9dae02481313e05fd9
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-12-19 10:38:47 +0000

    -

--- powershell/Modules/ModPlatformAD/ModPlatformADUsers.psm1
+++ powershell/Modules/ModPlatformAD/ModPlatformADUsers.psm1
@@ -159,7 +159,7 @@ function Add-ModPlatformADUsers {
     if ($ADUser) {
       Write-Debug "Updating User: $Username"
       Set-ADUser -Identity $Username -Credential $ModPlatformADCredential @Options
-      Set-ADAccountPassword -Identity $Username -Reset -NewPassword $Password
+      Set-ADAccountPassword -Identity $Username -Reset -NewPassword $Password -Credential $ModPlatformADCredential
     } elseif ($User.Value.ContainsKey("Password")) {
       $Password = $User.Value.Password
       Write-Output "Creating User: $Username"
