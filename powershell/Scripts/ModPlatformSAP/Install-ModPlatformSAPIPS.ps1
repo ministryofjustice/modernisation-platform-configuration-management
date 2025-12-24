8077360fc5690ae01fdde501390fc50634bf8530
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-12-24 10:52:16 +0000

    -

--- powershell/Scripts/ModPlatformSAP/Install-ModPlatformSAPIPS.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-ModPlatformSAPIPS.ps1
@@ -5,8 +5,7 @@ $ErrorActionPreference = "Stop"
 $SAPConfig  = Get-ModPlatformSAPConfig
 $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
 Get-SAPInstaller $SAPConfig.InstallPackages.Ips
-#Open-SAPInstaller $SAPConfig.InstallPackages.Ips
-
+Open-SAPInstaller $SAPConfig.InstallPackages.Ips
 Set-SAPEnvironmentVars $SAPConfig.Variables
 Copy-SAPResponseFile "../../" "response-install-ips.ini" $SAPConfig.InstallPackages.Ips $SAPConfig.Variables $SAPSecrets
 Install-IPS "response-install-ips.ini" $SAPConfig.InstallPackages.Ips $SAPSecrets
