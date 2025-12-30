a3531ab5672de2946e21abc969aa1569fa00e55e
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-12-30 14:23:32 +0000

    -

--- powershell/Scripts/ModPlatformSAP/Install-SAPIPSAsAdministrator.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPIPSAsAdministrator.ps1
@@ -5,3 +5,4 @@ $ErrorActionPreference = "Stop"
 $SAPConfig  = Get-ModPlatformSAPConfig
 $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
 Install-SAPIPS "response-install-ips.ini" $SAPConfig.InstallPackages.Ips $SAPSecrets
+Set-SAPIPSServiceControl $SAPConfig.Variables $SAPSecrets
