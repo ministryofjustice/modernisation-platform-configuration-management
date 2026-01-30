85e8c7c4429338f01c3ee7efc5ee499446f336bd
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-01-30 13:55:21 +0000

    -

--- powershell/Scripts/ModPlatformSAP/Install-SAPFlexiLogReader.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPFlexiLogReader.ps1
@@ -2,6 +2,7 @@ Import-Module ModPlatformSAP -Force
 
 $ErrorActionPreference = "Stop"
 
+. ../Common/Install-7Zip.ps1
 $SAPConfig  = Get-ModPlatformSAPConfig
 $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
 Get-SAPInstaller $SAPConfig.InstallPackages.FlexiLogReader
