d3d7221880713d5a871229ddc0db458090a32f65
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-01-30 15:04:34 +0000

    -

--- powershell/Scripts/ModPlatformSAP/Install-SAPFlexiLogReader.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPFlexiLogReader.ps1
@@ -7,3 +7,17 @@ $SAPConfig  = Get-ModPlatformSAPConfig
 $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
 Get-SAPInstaller $SAPConfig.InstallPackages.FlexiLogReader
 Open-SAPInstaller $SAPConfig.InstallPackages.FlexiLogReader
+
+# Create a shortcut
+$ShortcutDir = Join-Path -Path [Environment]::GetFolderPath('CommonDesktopDirectory') -ChildPath "4.3 Client Tools"
+if (-not (Test-Path $ShortcutDir)) {
+  Write-Output "Creating $ShortcutDir"
+  New-Item -ItemType Directory -Path $ShortcutDir -Force
+}
+$TargetPath = Join-Path -Path $SAPConfig.InstallPackages.FlexiLogReader.ExtractDir -ChildPath "FlexiLogReader64"
+$TargetPath = Join-Path -Path $TargetPath -ChildPath "FlexiLogReader64.exe"
+$ShortcutLink = Join-Path -Path $ShortcutDir -ChildPath "FlexiLogReader64.lnk"
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutPathLink)
+$Shortcut.TargetPath = $TargetPath
+$Shortcut.IconLocation = $TargetPath
+$Shortcut.Save()
