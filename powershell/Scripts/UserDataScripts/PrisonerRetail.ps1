4273a3d1e908e472bbbd08b7293d72330288f686
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-01-30 09:46:38 +0000

    TM-1866: update prison retail script (#1704)
    update prison retail script

--- powershell/Scripts/UserDataScripts/PrisonerRetail.ps1
+++ powershell/Scripts/UserDataScripts/PrisonerRetail.ps1
@@ -1,7 +1,31 @@
+$Scripts = @(
+    "../Common/Set-LocalFirewallOff.ps1",
+    "../Common/Set-IPv4Preferred.ps1",
+    "../Common/Set-TimezoneGMT.ps1",
+    "../Common/Install-Putty.ps1",
+    "../Common/Install-WinSCP.ps1",
+    "../Common/Get-PGPInstaller.ps1",
+    "../ModPlatformAD/Join-ModPlatformAD.ps1",
+    "../Microsoft/Remove-EdgeFirstRunExperience.ps1",
+    "../AmazonCloudWatchAgent/Install-AmazonCloudWatchAgent.ps1"
+)
+
 $ErrorActionPreference = "Stop"
-
-. ../ModPlatformAD/Join-ModPlatformAD.ps1
-
-if ($LASTEXITCODE -ne 0) {
-   Exit $LASTEXITCODE
+$ScriptDir = Get-Location
+$OverallExitCode = 0
+foreach ($Script in $Scripts) {
+  Write-Output "START $Script"
+  . $Script
+  if ($LASTEXITCODE -eq 3010) {
+    Write-Output "REBOOT REQUIRED after $Script - exiting with code 3010"
+    Exit 3010  # Exit immediately to allow reboot
+  }
+  if ($LASTEXITCODE -ne 0) {
+    $OverallExitCode = $LASTEXITCODE
+    Write-Output "ERROR $Script ExitCode=$LASTEXITCODE"
+  } else {
+    Write-Output "END $Script"
+  }
+  Set-Location $ScriptDir
 }
+Exit $OverallExitCode
