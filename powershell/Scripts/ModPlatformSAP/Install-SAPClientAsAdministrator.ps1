7ffdc253684f505856561528a3b5130369a7d826
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-02-11 16:15:54 +0000

    TM-1884: fix sap client shortcuts

--- powershell/Scripts/ModPlatformSAP/Install-SAPClientAsAdministrator.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPClientAsAdministrator.ps1
@@ -7,7 +7,7 @@ $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
 Set-SAPEnvironmentVars $SAPConfig.Variables
 Install-SAPClient "response-install-client.ini" $SAPConfig.InstallPackages.Client
 
-$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Designer.lnk"
+$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Designer (User Specific).lnk"
 if (-not (Test-Path $ShortcutPath)) {
   Write-Output "Creating Desktop Shortcut: $ShortcutPath"
   $WScriptShell = New-Object -ComObject WScript.Shell
@@ -16,13 +16,14 @@ if (-not (Test-Path $ShortcutPath)) {
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "win64_x64"
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "designer.exe"
   $Shortcut.TargetPath = $TargetPath
+  $Shortcut.Arguments = "-configuration '%USERPROFILE%\Documents'"
   $Shortcut.IconLocation = $TargetPath
   $Shortcut.Save()
 } else {
   Write-Output "Skipping Desktop Shortcut as $ShortcutPath already present"
 }
 
-$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Information Design Tool.lnk"
+$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Information Design Tool (User Specific).lnk"
 if (-not (Test-Path $ShortcutPath)) {
   Write-Output "Creating Desktop Shortcut: $ShortcutPath"
   $WScriptShell = New-Object -ComObject WScript.Shell
@@ -31,6 +32,7 @@ if (-not (Test-Path $ShortcutPath)) {
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "win64_x64"
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "InformationDesignTool.exe"
   $Shortcut.TargetPath = $TargetPath
+  $Shortcut.Arguments = "-configuration '%USERPROFILE%\Documents'"
   $Shortcut.IconLocation = $TargetPath
   $Shortcut.Save()
 } else {
