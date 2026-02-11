8e00329c79a023a4793aa7e5ce8c5fee4e431fe9
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-02-11 16:48:45 +0000

    -

--- powershell/Scripts/ModPlatformSAP/Install-SAPClientAsAdministrator.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPClientAsAdministrator.ps1
@@ -8,7 +8,7 @@ Set-SAPEnvironmentVars $SAPConfig.Variables
 Install-SAPClient "response-install-client.ini" $SAPConfig.InstallPackages.Client
 
 $ShortcutDir = Join-Path -Path ([Environment]::GetFolderPath('CommonDesktopDirectory')) -ChildPath "4.3 Client Tools"
-$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Designer (user specific config).lnk"
+$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Designer.lnk"
 if (-not (Test-Path $ShortcutPath)) {
   Write-Output "Creating Desktop Shortcut: $ShortcutPath"
   $WScriptShell = New-Object -ComObject WScript.Shell
@@ -17,14 +17,15 @@ if (-not (Test-Path $ShortcutPath)) {
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "win64_x64"
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "designer.exe"
   $Shortcut.TargetPath = $TargetPath
-  $Shortcut.Arguments = "-configuration %USERPROFILE%"
+  # Comment in to use a per-user configuration instead of global
+  # $Shortcut.Arguments = "-configuration %USERPROFILE%"
   $Shortcut.IconLocation = $TargetPath
   $Shortcut.Save()
 } else {
   Write-Output "Skipping Desktop Shortcut as $ShortcutPath already present"
 }
 
-$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Information Design Tool (user specific config).lnk"
+$ShortcutPath = Join-Path -Path $ShortcutDir -ChildPath "Information Design Tool.lnk"
 if (-not (Test-Path $ShortcutPath)) {
   Write-Output "Creating Desktop Shortcut: $ShortcutPath"
   $WScriptShell = New-Object -ComObject WScript.Shell
@@ -33,7 +34,8 @@ if (-not (Test-Path $ShortcutPath)) {
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "win64_x64"
   $TargetPath = Join-Path -Path $TargetPath -ChildPath "InformationDesignTool.exe"
   $Shortcut.TargetPath = $TargetPath
-  $Shortcut.Arguments = "-configuration %USERPROFILE%"
+  # Comment in to use a per-user configuration instead of global
+  # $Shortcut.Arguments = "-configuration %USERPROFILE%"
   $Shortcut.IconLocation = $TargetPath
   $Shortcut.Save()
 } else {
