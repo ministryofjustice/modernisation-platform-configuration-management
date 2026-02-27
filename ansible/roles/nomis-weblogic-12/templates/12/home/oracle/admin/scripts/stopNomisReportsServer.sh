bbe25ff584c399a35094886233a3ef8d28f6b336
ghill26 <George.Hill2@justice.gov.uk>
2026-02-27 13:49:53 +0000

    Update reports configuration

--- ansible/roles/nomis-weblogic-12/templates/12/home/oracle/admin/scripts/stopNomisReportsServer.sh
+++ ansible/roles/nomis-weblogic-12/templates/12/home/oracle/admin/scripts/stopNomisReportsServer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <reports_server_name>"
   exit 1
@@ -31,15 +33,62 @@ echo \"  )\" >> \"\${PY_LOC}\"\n\
 # END WLST NM CONNECT" "$FILE"
 }
 
+check_state() {
+  java weblogic.WLST <<EOF 2>/dev/null | \
+  grep -E "RUNNING|STARTING|RESTARTING|SHUTDOWN|UNKNOWN" | \
+  tail -1 | tr -d '\r'
+nmConnect(userConfigFile='$WLST_USERCONFIG',
+          userKeyFile='$WLST_USERKEY',
+          host='localhost',
+          port=5556,
+          domainName='nomis',
+          nmType='ssl')
+print nmServerStatus('$SERVER_NAME')
+exit()
+EOF
+}
+
 cleanup() {
-  FILE="$BIN_DIR/stopComponent.sh"
-  sed -i '/# BEGIN WLST NM CONNECT/,/# END WLST NM CONNECT/d' "$FILE"
+  sed -i '/# BEGIN WLST NM CONNECT/,/# END WLST NM CONNECT/d' "$BIN_DIR/stopComponent.sh"
   rm -f "$WLST_USERCONFIG" "$WLST_USERKEY"
 }
 
 generate_credentials
 inject_nm_block
-"$BIN_DIR/stopComponent.sh" "$SERVER_NAME"
-RETURN_VALUE=$?
+
+STATE=$(check_state)
+echo "Initial state: [$STATE]"
+
+if echo "$STATE" | grep -Eq "RUNNING|STARTING|RESTARTING"; then
+  echo "Stopping $SERVER_NAME..."
+  "$BIN_DIR/stopComponent.sh" "$SERVER_NAME"
+else
+  echo "$SERVER_NAME already stopped (state: $STATE)"
+fi
+
+echo "Waiting for $SERVER_NAME to fully shutdown..."
+
+MAX_WAIT=300
+WAITED=0
+
+while true; do
+  STATE=$(check_state)
+  echo "Current state: [$STATE]"
+
+  if ! echo "$STATE" | grep -Eq "RUNNING|STARTING|RESTARTING"; then
+    echo "$SERVER_NAME is fully stopped."
+    break
+  fi
+
+  sleep 5
+  WAITED=$((WAITED+5))
+
+  if [ $WAITED -ge $MAX_WAIT ]; then
+    echo "Timeout waiting for shutdown!"
+    cleanup
+    exit 1
+  fi
+done
+
 cleanup
-exit $RETURN_VALUE
+exit 0
