d56d5dd0e88a646883e9e0181c301fca07be0293
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-04-09 13:47:41 +0100

    -

--- powershell/Scripts/ModPlatformSAP/Install-SAPBIP.ps1
+++ powershell/Scripts/ModPlatformSAP/Install-SAPBIP.ps1
@@ -4,6 +4,6 @@ $ErrorActionPreference = "Stop"
 
 $SAPConfig  = Get-ModPlatformSAPConfig
 $SAPSecrets = Get-ModPlatformSAPSecrets $SAPConfig
-Get-SAPInstaller $SAPConfig.InstallPackages.Bip
-Expand-SAPInstaller $SAPConfig.InstallPackages.Bip
+#Get-SAPInstaller $SAPConfig.InstallPackages.Bip
+#Expand-SAPInstaller $SAPConfig.InstallPackages.Bip
 Copy-SAPResponseFile "../../" "response-install-bip.ini" $SAPConfig.InstallPackages.Bip $SAPConfig.Variables $SAPSecrets
