a4ae3bf5e62950bdec067f44a621ee290514a494
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2026-09-02 09:52:02 +0100

    Use TNS Alias for Reporting

--- ansible/roles/oracle-db-rat-report/files/generate_rat_report.sh
+++ ansible/roles/oracle-db-rat-report/files/generate_rat_report.sh
@@ -13,14 +13,9 @@ report_directory_path="$2"
 report_filename="$3"
 replay_name="$4"
 tns_alias="$5"
-target_db_name="${TARGET_DB_NAME:-${ORACLE_SID:-}}"
+
 rat_secret_id="${RAT_SECRET_ID:-}"
 aws_region="${AWS_REGION:-}"
-
-if [[ -z "${target_db_name}" ]]; then
-  echo "Set TARGET_DB_NAME or ORACLE_SID before running this script." >&2
-  exit 1
-fi
 
 if [[ -z "${tns_alias}" ]]; then
   echo "Set tns_alias before running this script." >&2
@@ -36,7 +31,7 @@ echo "Report directory name: ${report_directory_name}"
 echo "Report directory path: ${report_directory_path}"
 echo "Report filename: ${report_filename}"
 echo "Replay name: ${replay_name}"
-echo "Target database name: ${target_db_name}"
+echo "Target database name: ${tns_alias}"
 
 export PATH="$PATH:/usr/local/bin"
 rat_replay_password="$(aws secretsmanager get-secret-value \
@@ -49,10 +44,6 @@ if [[ -z "${rat_replay_password}" ]]; then
   echo "RAT_REPLAY password is empty in secret ${rat_secret_id}." >&2
   exit 1
 fi
-
-export ORAENV_ASK=NO
-export ORACLE_SID="${target_db_name}"
-. oraenv -s
 
 sqlplus -s /nolog <<EOF
 whenever sqlerror exit failure
